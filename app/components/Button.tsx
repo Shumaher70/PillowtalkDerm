49c1ac7503b143d8643b1a9e73c2cb221d420d02
oleg <olegvraby@gmail.com>
2023-11-01 13:36:38 +0200

    create a custom bottom component

---
+++ app/components/Button.tsx
@@ -0,0 +1,39 @@
+interface ButtonProps {
+   text: string;
+   uppercase?: boolean;
+   bg?: boolean;
+   size: 'sm' | 'lg';
+   onClick?: () => void;
+   className?: string;
+}
+
+const Button = ({
+   text = 'button',
+   uppercase,
+   bg,
+   size = 'sm',
+   className,
+   onClick,
+}: ButtonProps) => {
+   return (
+      <button
+         onClick={onClick}
+         className={`
+            ${size === 'lg' && 'lg:px-[24px] lg:py-[12px] px-[24px] py-[10px]'}
+            ${size === 'sm' && 'px-[16px] py-[8px]'}
+            p-[10px]
+            text-[14px]
+            lg:text-[16px]
+            rounded-full
+            text-white
+            ${bg ? 'bg-gradient' : ''}
+            ${uppercase ? 'uppercase' : 'capitalize'}
+            ${className}
+         `}
+      >
+         {text}
+      </button>
+   );
+};
+
+export default Button;
