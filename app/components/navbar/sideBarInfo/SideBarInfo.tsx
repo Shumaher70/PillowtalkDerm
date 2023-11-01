3fd63b0bc440c4db6f1aa0e2a08a0cc1086d1e98
oleg <olegvraby@gmail.com>
2023-11-01 14:43:43 +0200

    fixing animations in SideBarInfo

--- app/components/navbar/sideBarInfo/SideBarInfo.tsx
+++ app/components/navbar/sideBarInfo/SideBarInfo.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useState } from 'react';
-import { motion } from 'framer-motion';
+import { LazyMotion, domAnimation, m } from 'framer-motion';
 import { AiOutlineClose } from 'react-icons/ai';
 
 import Button from '../../Button';
@@ -11,8 +11,8 @@ interface SideBarInfoProps {
 }
 
 const sideBar = {
-   open: { x: 0 },
-   closed: { x: '-100%' },
+   open: { x: '100%' },
+   closed: { x: '0' },
 };
 
 const SideBarInfo = ({
@@ -32,11 +32,12 @@ const SideBarInfo = ({
    };
 
    return (
-      <motion.div
-         animate={triggerSideBar ? 'open' : 'closed'}
-         transition={{ duration: 0.5 }}
-         variants={sideBar}
-         className="
+      <LazyMotion features={domAnimation}>
+         <m.div
+            animate={triggerSideBar ? 'open' : 'closed'}
+            transition={{ duration: 0.5 }}
+            variants={sideBar}
+            className="
             w-full 
             h-full 
             flex
@@ -46,37 +47,42 @@ const SideBarInfo = ({
             overflow-y-auto
             fixed
             top-0
-            right-0
+            right-[100%]
             lg:hidden
          "
-      >
-         <div className="p-[16px] flex justify-between items-center">
-            <div className="flex flex-wrap gap-3 z-10">
-               <Button
-                  text="shop"
-                  size="sm"
-                  className={`${
-                     shop ? 'text-white bg-gradient' : 'text-black bg-white'
-                  }`}
-                  onClick={shopHandler}
-               />
-               <Button
-                  text="skin nerd academy"
-                  size="sm"
-                  className={`${
-                     blog ? 'text-white bg-gradient' : 'text-black bg-white'
-                  }`}
-                  onClick={blogHandler}
-               />
-               <Button text="about" size="sm" className="bg-white text-black" />
-            </div>
+         >
+            <div className="p-[16px] flex justify-between items-center">
+               <div className="flex flex-wrap gap-3 z-10">
+                  <Button
+                     text="shop"
+                     size="sm"
+                     className={`${
+                        shop ? 'text-white bg-gradient' : 'text-black bg-white'
+                     }`}
+                     onClick={shopHandler}
+                  />
+                  <Button
+                     text="skin nerd academy"
+                     size="sm"
+                     className={`${
+                        blog ? 'text-white bg-gradient' : 'text-black bg-white'
+                     }`}
+                     onClick={blogHandler}
+                  />
+                  <Button
+                     text="about"
+                     size="sm"
+                     className="bg-white text-black"
+                  />
+               </div>
 
-            <AiOutlineClose
-               onClick={triggerSideBarHandler}
-               className="w-[15px] h-[15px] cursor-pointer"
-            />
-         </div>
-      </motion.div>
+               <AiOutlineClose
+                  onClick={triggerSideBarHandler}
+                  className="w-[15px] h-[15px] cursor-pointer"
+               />
+            </div>
+         </m.div>
+      </LazyMotion>
    );
 };
 
