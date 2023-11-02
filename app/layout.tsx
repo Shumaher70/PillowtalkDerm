8fb6877ee852dce1e1684f1b593e4fc22ff636c2
oleg <olegvraby@gmail.com>
2023-11-02 15:17:12 +0200

    add:layout.tsx: add localFont

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,58 @@
 import type { Metadata } from 'next';
+
 import './globals.css';
-import { neueHaasUnicaFont } from '@/public/fonts/variables';
+
 import Navbar from './components/navbar/Navbar';
+
+import localFont from 'next/font/local';
+
+export const neueHaasUnicaFont = localFont({
+   src: [
+      {
+         path: '../public/fonts/NeueHaasUnica-Bold.woff2',
+         weight: '700',
+      },
+      {
+         path: '../public/fonts/NeueHaasUnica-BoldItalic.woff2',
+         weight: '700',
+      },
+      {
+         path: '../public/fonts/NeueHaasUnica-Medium.woff2',
+         weight: '500',
+      },
+      {
+         path: '../public/fonts/NeueHaasUnica-MediumItalic.woff2',
+         weight: '500',
+      },
+      {
+         path: '../public/fonts/NeueHaasUnica-Regular.woff2',
+         weight: '400',
+      },
+      {
+         path: '../public/fonts/NeueHaasUnica-Light.woff2',
+         weight: '300',
+      },
+      {
+         path: '../public/fonts/NeueHaasUnica-LightItalic.woff2',
+         weight: '300',
+      },
+      {
+         path: '../public/fonts/NeueHaasUnica-Thin.woff2',
+         weight: '100',
+      },
+      {
+         path: '../public/fonts/NeueHaasUnica-ThinItalic.woff2',
+         weight: '100',
+      },
+   ],
+   variable: '--font-neueHaasUnica',
+});
+
+export const schnyderMlightFont = localFont({
+   src: '../public/fonts/SchnyderMLight.woff2',
+   weight: '100',
+   variable: '--font-schnyderMlightFont',
+});
 
 export const metadata: Metadata = {
    title: 'PillowTalkDerm',
