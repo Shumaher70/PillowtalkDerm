3b6c43523e1135279ed6f3322e5ad5861aa22e64
oleg <olegvraby@gmail.com>
2023-11-06 14:56:59 +0200

    add:app/page:metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+   title: 'PillowTalkDerm',
+   description: 'PillowTalkDerm shop',
+};
+
 export default function Home() {
    return <main className="px-[16px] lg:px-[80px]">hello</main>;
 }
