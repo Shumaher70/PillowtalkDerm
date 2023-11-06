4e09f318a988075f93e37170abe4c49f9f613a41
oleg <olegvraby@gmail.com>
2023-11-06 10:04:58 +0200

    add:Card.tsx

---
+++ app/components/navbar/sideBarInfo/components/card/Card.tsx
@@ -0,0 +1,81 @@
+import Link from 'next/link';
+import ImageCard from './components/ImageCard';
+import TitleCard from './components/TitleCard';
+import ReadMe from './components/ReadMe';
+import Stars from './components/Stars';
+import Rating from './components/Rating';
+import Button from '@/app/components/Button';
+import AwardWining from './components/AwardWinning';
+
+interface CardProps {
+   href: string;
+   extra?: boolean;
+   title: string;
+   image: string;
+   stars?: string[];
+   rating?: string;
+   btn?: boolean;
+   win?: boolean;
+}
+
+const Card = ({
+   href,
+   stars,
+   rating,
+   btn,
+   win,
+   extra,
+   title,
+   image,
+}: CardProps) => {
+   return (
+      <Link href={href} className="relative bg-white rounded-[8px] m-2">
+         <div className="m-[10px] rounded-[5px]">
+            <ImageCard image={image} title={title} />
+
+            {stars && (
+               <div className="flex flex-center gap-1">
+                  {Array.from({ length: 5 }).map(() => (
+                     <>{<Stars />}</>
+                  ))}
+               </div>
+            )}
+
+            {rating && (
+               <div className="flex flex-center">
+                  <Rating />
+               </div>
+            )}
+
+            <div className="pt-[8px] text-center">
+               <TitleCard title={title} />
+            </div>
+
+            {extra && (
+               <div className="text-center pt-[8px]">
+                  <ReadMe extra={extra} />
+               </div>
+            )}
+
+            {btn && (
+               <div className="flex w-full flex-center pt-[8px]">
+                  <Button
+                     text="add-$58"
+                     size="sm"
+                     uppercase
+                     className="bg-purple !w-full"
+                  />
+               </div>
+            )}
+         </div>
+
+         {win && (
+            <div className="absolute top-3 left-3">
+               <AwardWining />
+            </div>
+         )}
+      </Link>
+   );
+};
+
+export default Card;
