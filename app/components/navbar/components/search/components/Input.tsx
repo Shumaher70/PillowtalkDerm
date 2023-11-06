c9a859a3ce229ec1be1ed9559f6a7cb588c3aae3
oleg <olegvraby@gmail.com>
2023-11-06 15:08:39 +0200

    create:input.tsx&TopSearches

---
+++ app/components/navbar/components/search/components/Input.tsx
@@ -0,0 +1,89 @@
+'use client';
+import { useEffect, useState } from 'react';
+import { CiSearch } from 'react-icons/ci';
+
+interface InputProps {
+   getInput?: (event: string) => void;
+   postInput?: string;
+}
+
+const Input = ({ getInput, postInput }: InputProps) => {
+   const post = postInput || '';
+
+   const [outline, setOutline] = useState(false);
+   const [input, setInput] = useState(post);
+
+   const inputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setInput(event.target.value);
+   };
+
+   useEffect(() => {
+      if (getInput) {
+         const timer = setTimeout(() => {
+            getInput(input);
+         }, 300);
+         return () => clearTimeout(timer);
+      }
+   }, [getInput, input]);
+
+   useEffect(() => {
+      if (post) return setInput(post);
+   }, [post]);
+
+   const onFocus = () => setOutline(true);
+   const onBlur = () => setOutline(false);
+
+   return (
+      <div
+         className="
+         w-full 
+         flex 
+         flex-center 
+         rounded-full 
+         bg-white 
+         border-1 
+         border-[#cfb6e2] 
+         py-[6px] 
+         pr-[6px] 
+         pl-[16px] 
+         gap-4 
+         relative
+         "
+      >
+         {outline && (
+            <div
+               className="
+               absolute 
+               top-[-7] 
+               right-[-7px]
+               w-[calc(100%_+_14px)] 
+               h-[calc(100%_+_14px)] 
+               rounded-full
+               border-2 
+               border-[#cfb6e2]
+               pointer-events-none
+               "
+            />
+         )}
+
+         <CiSearch className={`text-[20px] cursor-pointer `} />
+         <input
+            value={input}
+            type="text"
+            placeholder="Search"
+            className="w-full bg-inherit outline-none text-[12px]"
+            onFocus={onFocus}
+            onBlur={onBlur}
+            onChange={inputHandler}
+         />
+
+         {input.length > 0 && (
+            <p className="mr-[7px] cursor-pointer" onClick={() => setInput('')}>
+               Clear
+            </p>
+         )}
+      </div>
+   );
+};
+
+export default Input;
---
+++ app/components/navbar/components/search/components/TopSearches.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { schnyderMlightFont } from '@/app/layout';
+
+interface TopSearchesProps {
+   className?: string;
+   getTopSearch?: (event: string) => void;
+}
+
+const topSearches = ['major fade', 'ingredients', 'skincare', 'lightweight'];
+
+const TopSearches = ({ className, getTopSearch }: TopSearchesProps) => {
+   const clickHandler = (event: React.MouseEvent<HTMLParagraphElement>) => {
+      getTopSearch?.(event.currentTarget.innerText);
+   };
+
+   return (
+      <div className={`${className}`}>
+         <h4
+            className={`${schnyderMlightFont.className} text-[28px] !font-[300]`}
+         >
+            Top Searches
+         </h4>
+
+         <div className="grid grid-cols-2 space-y-2 mt-3">
+            {topSearches.map((search, index) => (
+               <p
+                  key={index}
+                  className="text-p cursor-pointer"
+                  onClick={clickHandler}
+               >
+                  {search}
+               </p>
+            ))}
+         </div>
+      </div>
+   );
+};
+
+export default TopSearches;
