83f81e9d43bbd263ca09078e82895085c190376a
oleg <olegvraby@gmail.com>
2023-11-07 15:19:30 +0200

    create:progressTitle

---
+++ utils/progressTitle.tsx
@@ -0,0 +1,11 @@
+const progressTitle = (totalPrice: number, freeShipping: number) => {
+   if (totalPrice >= freeShipping) {
+      return 'free shipping!';
+   } else if (totalPrice > 0 && totalPrice < freeShipping) {
+      return `you are $${freeShipping - totalPrice} away from free shipping.`;
+   } else {
+      return 'You are $135 away from free shipping.';
+   }
+};
+
+export default progressTitle;
