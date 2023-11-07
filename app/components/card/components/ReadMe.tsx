bb3537ce765b828d0adc4d98c1700cea28ff52f5
oleg <olegvraby@gmail.com>
2023-11-07 15:10:14 +0200

    change:ReadMe: remove prop "extra"

--- app/components/card/components/ReadMe.tsx
+++ app/components/card/components/ReadMe.tsx
@@ -1,13 +1,5 @@
-const ReadMe = ({ extra }: { extra?: boolean }) => {
-   return (
-      <>
-         {extra && (
-            <p className="text-[12px] underline hover:no-underline">
-               Read More
-            </p>
-         )}
-      </>
-   );
+const ReadMe = ({ text = 'Read me' }: { extra?: boolean; text?: string }) => {
+   return <p className="text-[12px] underline hover:no-underline">{text}</p>;
 };
 
 export default ReadMe;
