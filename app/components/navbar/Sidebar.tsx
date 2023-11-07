534ecd0a6dba26b4036d025ece3cf1df094cf26f
oleg <olegvraby@gmail.com>
2023-11-07 15:14:13 +0200

    add:Sidebar: add props and a condition "left-right"

--- app/components/navbar/Sidebar.tsx
+++ app/components/navbar/Sidebar.tsx
@@ -3,21 +3,28 @@ import { LazyMotion, domAnimation, m } from 'framer-motion';
 interface SidebarProps {
    triggerSidebar: boolean;
    children: React.ReactNode;
+   left?: boolean;
+   className?: string;
 }
 
-const sideBar = {
-   open: { x: '100%' },
-   closed: { x: '0' },
-};
+const Sidebar = ({
+   triggerSidebar,
+   children,
+   left,
+   className,
+}: SidebarProps) => {
+   const sideBar = {
+      open: left ? { x: '-100%' } : { x: '100%' },
+      closed: { x: '0' },
+   };
 
-const Sidebar = ({ triggerSidebar, children }: SidebarProps) => {
    return (
       <LazyMotion features={domAnimation}>
          <m.div
             animate={triggerSidebar ? 'open' : 'closed'}
             transition={{ duration: 0.5 }}
             variants={sideBar}
-            className="
+            className={`
             w-full 
             h-full 
             overflow-hidden
@@ -27,9 +34,10 @@ const Sidebar = ({ triggerSidebar, children }: SidebarProps) => {
             z-10
             fixed
             top-0
-            right-[100%]
+            ${left ? 'left-[100%]' : 'right-[100%]'}
             lg:hidden
-         "
+            ${className}
+         `}
          >
             {children}
          </m.div>
