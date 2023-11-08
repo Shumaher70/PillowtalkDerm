ce97c47d8af3368360850dcf0d2efc5d56aba027
oleg <olegvraby@gmail.com>
2023-11-08 14:52:35 +0200

    change:layout:remove font "neueHaasUnicaFont"

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,48 +4,6 @@ import Navbar from './components/navbar/Navbar';
 
 import localFont from 'next/font/local';
 
-export const neueHaasUnicaFont = localFont({
-   src: [
-      {
-         path: '../public/fonts/NeueHaasUnica-Bold.woff2',
-         weight: '700',
-      },
-      {
-         path: '../public/fonts/NeueHaasUnica-BoldItalic.woff2',
-         weight: '700',
-      },
-      {
-         path: '../public/fonts/NeueHaasUnica-Medium.woff2',
-         weight: '500',
-      },
-      {
-         path: '../public/fonts/NeueHaasUnica-MediumItalic.woff2',
-         weight: '500',
-      },
-      {
-         path: '../public/fonts/NeueHaasUnica-Regular.woff2',
-         weight: '400',
-      },
-      {
-         path: '../public/fonts/NeueHaasUnica-Light.woff2',
-         weight: '300',
-      },
-      {
-         path: '../public/fonts/NeueHaasUnica-LightItalic.woff2',
-         weight: '300',
-      },
-      {
-         path: '../public/fonts/NeueHaasUnica-Thin.woff2',
-         weight: '100',
-      },
-      {
-         path: '../public/fonts/NeueHaasUnica-ThinItalic.woff2',
-         weight: '100',
-      },
-   ],
-   variable: '--font-neueHaasUnica',
-});
-
 export const schnyderMlightFont = localFont({
    src: '../public/fonts/SchnyderMLight.woff2',
    weight: '100',
@@ -59,7 +17,7 @@ export default function RootLayout({
 }) {
    return (
       <html lang="en">
-         <body className={`${neueHaasUnicaFont.className} font-sans`}>
+         <body>
             <Navbar />
             {children}
          </body>
