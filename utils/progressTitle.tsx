fbbce8a6450dcc1440b7c996847413fc5c9e8a80
oleg <olegvraby@gmail.com>
2023-11-08 15:04:12 +0200

    change:progressTitle: Capitalize the letter

--- utils/progressTitle.tsx
+++ utils/progressTitle.tsx
@@ -2,7 +2,7 @@ const progressTitle = (totalPrice: number, freeShipping: number) => {
    if (totalPrice >= freeShipping) {
       return 'free shipping!';
    } else if (totalPrice > 0 && totalPrice < freeShipping) {
-      return `you are $${freeShipping - totalPrice} away from free shipping.`;
+      return `You are $${freeShipping - totalPrice} away from free shipping.`;
    } else {
       return 'You are $135 away from free shipping.';
    }
