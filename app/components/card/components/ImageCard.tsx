ee2136f24536343297826059751abf0c106d8732
oleg <olegvraby@gmail.com>
2023-11-09 11:57:39 +0200

    add:ImageCard: dynamic alt

--- app/components/card/components/ImageCard.tsx
+++ app/components/card/components/ImageCard.tsx
@@ -5,12 +5,13 @@ const ImageCard = ({ image, title }: { image: string; title: string }) => {
       <div className="relative pt-[100%] rounded-[10px] overflow-hidden">
          <Image
             src={image}
-            alt="pillow"
+            alt={title}
             className="w-full h-full object-cover top-0 absolute"
             sizes="100vw"
             width={0}
             height={0}
             loading="lazy"
+            draggable="false"
          />
       </div>
    );
