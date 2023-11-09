06b902845a226eb3815640a4553055191cd44739
oleg <olegvraby@gmail.com>
2023-11-09 11:59:54 +0200

    add:CarouselCart: the custom dots and remove the arrows

--- app/components/navbar/components/cart/components/carouselCart/CarouselCart.tsx
+++ app/components/navbar/components/cart/components/carouselCart/CarouselCart.tsx
@@ -1,5 +1,8 @@
+import { Children } from 'react';
+
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
+
 import ProductInCart from '../productInCart/ProductInCart';
 
 const CarouselCart = ({ data }: { data: string[] }) => {
@@ -11,9 +14,27 @@ const CarouselCart = ({ data }: { data: string[] }) => {
       },
    };
 
+   const CustomDot = ({
+      onClick,
+      active,
+   }: {
+      onClick: () => void;
+      active: boolean;
+   }) => {
+      return (
+         <button
+            className={`${
+               active ? 'bg-gradient' : 'inactive'
+            } w-[8px] h-[8px] border-1 border-[#F32E79] rounded-full m-1`}
+            onClick={() => onClick()}
+         ></button>
+      );
+   };
+
    return (
       <Carousel
          swipeable={true}
+         arrows={false}
          draggable={true}
          showDots={true}
          responsive={responsive}
@@ -22,8 +43,16 @@ const CarouselCart = ({ data }: { data: string[] }) => {
          keyBoardControl={true}
          transitionDuration={10}
          containerClass="carousel-container"
-         dotListClass="custom-dot-list-style"
-         itemClass="carousel-item-padding-40-px"
+         dotListClass="block w-max absolute -top-[19px] left-[100%] -translate-x-full"
+         itemClass="p-2"
+         className="overflow-visible"
+         customDot={
+            data.length > 1 ? (
+               <CustomDot onClick={() => {}} active={false} />
+            ) : (
+               <></>
+            )
+         }
       >
          {data.map((item, index) => (
             <ProductInCart
