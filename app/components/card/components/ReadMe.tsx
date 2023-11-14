e8116a837c290d46ac8b3a2ca320510608288ecf
oleg <olegvraby@gmail.com>
2023-11-14 17:56:47 +0200

    add:ReadMe: add prop src

--- app/components/card/components/ReadMe.tsx
+++ app/components/card/components/ReadMe.tsx
@@ -1,5 +1,17 @@
-const ReadMe = ({ text = 'Read me' }: { text?: string }) => {
-   return <p className="text-[12px] underline hover:no-underline">{text}</p>;
+import Link from 'next/link';
+
+const ReadMe = ({
+   text = 'Read me',
+   src = '',
+}: {
+   text?: string;
+   src: string;
+}) => {
+   return (
+      <Link href={src} className="text-[12px] underline hover:no-underline">
+         {text}
+      </Link>
+   );
 };
 
 export default ReadMe;
