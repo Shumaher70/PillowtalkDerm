379f1b069435f4a521b13221f31e666e8a2c005b
oleg <olegvraby@gmail.com>
2023-11-14 10:18:30 +0200

    add:calcRatingStars: logic

--- utils/calcRatingStars.ts
+++ utils/calcRatingStars.ts
@@ -1,5 +1,10 @@
-const calcRatingStars = (TotalQuantity: string[]) => {
-   const quantityReviews = TotalQuantity.length;
+import { Review } from '@prisma/client';
+
+const calcRatingStars = (TotalQuantity: number, totalStars: Review[]) => {
+   const ratingStars = totalStars.map((review) => review.rating);
+   const totalRatingStars = ratingStars.reduce((acc, sum) => acc + sum, 0);
+
+   return +totalRatingStars.toFixed(2) / TotalQuantity;
 };
 
 export default calcRatingStars;
