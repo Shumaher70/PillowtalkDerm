1b5250d6fc6587646115f50b22e37b03351f1f12
oleg <olegvraby@gmail.com>
2023-11-14 10:25:20 +0200

    change:Stars.tsx: logic

--- app/components/card/components/Stars.tsx
+++ app/components/card/components/Stars.tsx
@@ -1,16 +1,30 @@
-import { AiFillStar } from 'react-icons/ai';
+import { IoIosStarHalf, IoIosStarOutline, IoMdStar } from 'react-icons/io';
 
 interface StarsProps {
-   stars: string[];
+   stars: number;
 }
 
 const Stars = ({ stars }: StarsProps) => {
+   const starts = [];
+   for (let i = 0; i < 5; i++) {
+      const difference = parseFloat((stars - i).toFixed(1));
+      if (difference >= 1)
+         starts.push(<IoMdStar className="text-pink-700 w-5 h-5" />);
+      else if (difference < 1 && difference > 0) {
+         if (difference <= 0.2)
+            starts.push(<IoIosStarOutline className="text-pink-700 w-5 h-5" />);
+         else if (difference > 0.2 && difference <= 0.6)
+            starts.push(<IoIosStarHalf className="text-pink-700 w-5 h-5" />);
+         else starts.push(<IoMdStar className="text-pink-700 w-5 h-5" />);
+      } else
+         starts.push(<IoIosStarOutline className="text-pink-700 w-5 h-5" />);
+   }
+
    return (
       <div className="flex gap-1">
-         {stars &&
-            stars.map((_, index) => (
-               <AiFillStar key={index} className="text-pink-700 w-4 h-4" />
-            ))}
+         {starts.map((star, index) => {
+            return star;
+         })}
       </div>
    );
 };
