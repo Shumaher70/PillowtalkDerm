71c8487c8201060427561ccee5afb801e9b34e1e
oleg <olegvraby@gmail.com>
2023-11-18 08:06:10 +0200

    add:products/route: include reviews in the prisma search

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -2,7 +2,12 @@ import { prisma } from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
-   const products = await prisma.product.findMany();
+   const products = await prisma.product.findMany({
+      include: {
+         reviews: true,
+      },
+   });
+
    return NextResponse.json(products);
 }
 
