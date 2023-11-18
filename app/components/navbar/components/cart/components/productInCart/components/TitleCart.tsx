080b1bd04718f28547c76a53f2551a9bafd4cf05
oleg <olegvraby@gmail.com>
2023-11-18 08:35:27 +0200

    change:TitleCart: to change the text size and add capitalize

--- app/components/navbar/components/cart/components/productInCart/components/TitleCart.tsx
+++ app/components/navbar/components/cart/components/productInCart/components/TitleCart.tsx
@@ -1,5 +1,9 @@
 const TitleCart = ({ title }: { title: string }) => {
-   return <p className="text-p leading-[20px] font-[500] font-sans">{title}</p>;
+   return (
+      <p className="text-[14px] leading-[20px] font-[500] font-sans capitalize">
+         {title}
+      </p>
+   );
 };
 
 export default TitleCart;
