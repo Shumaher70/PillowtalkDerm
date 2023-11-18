91989e26b0d048278e187b2f55c662cd42bea6c6
oleg <olegvraby@gmail.com>
2023-11-18 08:07:10 +0200

    add:Button: disabled

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -7,6 +7,7 @@ interface ButtonProps {
    className?: string;
    soldOut?: boolean;
    children?: React.ReactNode;
+   disabled?: boolean;
 }
 
 const Button = ({
@@ -17,27 +18,30 @@ const Button = ({
    className,
    onClick,
    soldOut,
+   disabled,
 }: ButtonProps) => {
    return (
       <button
          onClick={onClick}
-         disabled={soldOut ? true : false}
+         disabled={soldOut || disabled ? true : false}
          className={`
-            ${size === 'lg' && 'lg:px-[24px] lg:py-[12px] px-[24px] py-[10px]'}
-            ${size === 'sm' && 'px-[16px] py-[8px]'}
-            p-[10px]
-            text-[14px]
-            lg:text-[16px]
-            rounded-full
-            w-max
-            h-max
-            flex
-            flex-center
-            text-white
-            ${bg ? 'bg-gradient' : ''}
-            ${uppercase ? 'uppercase' : 'capitalize'}
-            ${soldOut && 'cursor-not-allowed'}
-            ${className}
+         ${size === 'lg' && 'lg:px-[24px] lg:py-[12px] px-[24px] py-[10px]'}
+         ${size === 'sm' && 'px-[16px] py-[8px]'}
+         p-[10px]
+         text-[14px]
+         lg:text-[16px]
+         rounded-full
+         w-max
+         h-max
+         flex
+         flex-center
+         text-white
+         ${disabled ? 'opacity-50' : ''}
+         ${bg ? 'bg-gradient' : ''}
+         ${uppercase ? 'uppercase' : 'capitalize'}
+         ${soldOut && 'cursor-not-allowed'}
+         ${disabled && 'cursor-not-allowed'}
+         ${className}   
          `}
       >
          {!soldOut ? text : 'sold out'}
