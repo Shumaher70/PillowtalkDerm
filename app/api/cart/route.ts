ada846b7fab723d0a7d94869e53b41b40ad44ff6
oleg <olegvraby@gmail.com>
2023-11-18 08:04:37 +0200

    change:cart/route.ts: remove the product count logic

--- app/api/cart/route.ts
+++ app/api/cart/route.ts
@@ -10,38 +10,12 @@ export const POST = async (request: NextRequest) => {
    try {
       const body = await request.json();
 
-      const productId = await prisma.cart.findMany({
-         where: {
-            productId: body.productId,
-         },
+      const cart = await prisma.cart.create({
+         data: body,
       });
-
-      if (productId.length > 0) {
-         const quantity = (productId[0].quantity += 1);
-         const totalPrice = (productId[0].totalPrice +=
-            productId[0].totalPrice);
-         const cart = await prisma.cart.update({
-            where: {
-               id: productId[0].id,
-            },
-            data: {
-               quantity: quantity,
-               totalPrice: totalPrice,
-            },
-         });
-
-         return new NextResponse(JSON.stringify(cart), {
-            status: 200,
-         });
-      } else {
-         const cart = await prisma.cart.create({
-            data: body,
-         });
-
-         return new NextResponse(JSON.stringify(cart), {
-            status: 200,
-         });
-      }
+      return new NextResponse(JSON.stringify(cart), {
+         status: 200,
+      });
    } catch (error: any) {
       return new NextResponse(error.message, { status: 500 });
    }
