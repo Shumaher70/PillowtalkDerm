1b07a9506ce1723882ef0257fd2b4cf243afd330
oleg <olegvraby@gmail.com>
2023-11-20 18:53:27 +0200

    add:Card: Button

--- app/components/card/Card.tsx
+++ app/components/card/Card.tsx
@@ -12,7 +12,7 @@ import TitleCard from './components/TitleCard';
 import ReadMe from './components/ReadMe';
 import Stars from './components/Stars';
 import Rating from './components/Rating';
-import Button from '@/app/components/Button';
+import Button from '@/app/components/button/Button';
 import AwardWining from './components/AwardWinning';
 import calcRatingStars from '@/utils/calcRatingStars';
 import { useState } from 'react';
@@ -63,31 +63,6 @@ const Card = ({ btn, win, product, extra, stars, rating }: CardProps) => {
       }, 500);
    };
 
-   const spin = (
-      <>
-         <svg
-            className="animate-spin -ml-1 mr-3 h-[21px] w-[21px] text-white"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-         >
-            <circle
-               className="opacity-25"
-               cx="12"
-               cy="12"
-               r="10"
-               stroke="currentColor"
-               strokeWidth="4"
-            ></circle>
-            <path
-               className="opacity-75"
-               fill="currentColor"
-               d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            ></path>
-         </svg>
-      </>
-   );
-
    return (
       <Link href={''} className="relative bg-white rounded-[8px] m-2">
          <div className="m-[10px] rounded-[5px]">
@@ -118,11 +93,11 @@ const Card = ({ btn, win, product, extra, stars, rating }: CardProps) => {
             {btn && (
                <div className="flex w-full flex-center pt-[8px]">
                   <Button
-                     text={load ? spin : `add - $${product.price}`}
-                     disabled={load ? true : false}
+                     text={`add - $${product.price}`}
                      size="sm"
+                     load={load}
                      uppercase
-                     className="bg-purple-800 !w-full uppercase active:bg-gradient-to-r from-pink-400 to-pink-600 "
+                     className="bg-purple-800 uppercase active:bg-gradient-to-r from-pink-400 to-pink-600 "
                      soldOut={product.soldOut}
                      onClick={clickHandler}
                   />
