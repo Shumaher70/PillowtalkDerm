1dc5a09780a53200d4e603c7b589eafed058fc9c
oleg <olegvraby@gmail.com>
2023-11-20 19:04:03 +0200

    add:ProductInCart: add the dispatch action "addCart" and a div wrapper for the button

--- app/components/navbar/components/cart/components/productInCart/ProductInCart.tsx
+++ app/components/navbar/components/cart/components/productInCart/ProductInCart.tsx
@@ -1,15 +1,21 @@
+'use client';
+
 import Stars from '@/app/components/card/components/Stars';
 
 import TitleCart from './components/TitleCart';
 import ReadMe from '@/app/components/card/components/ReadMe';
 import ImageCard from '@/app/components/card/components/ImageCard';
 import CountProduct from './components/CountProduct';
-import Button from '@/app/components/Button';
+import Button from '@/app/components/button/Button';
 import { AiOutlineClose } from 'react-icons/ai';
 import { Review } from '@prisma/client';
 import calcRatingStars from '@/utils/calcRatingStars';
+import { useAppDispatch } from '@/redux/hooks';
+import { addCart } from '@/redux/features/cartSlice';
+import { useState } from 'react';
 
 interface ProductInCartProps {
+   id: string;
    stars?: boolean;
    image: string;
    reviews: Review[];
@@ -22,9 +28,12 @@ interface ProductInCartProps {
    button?: boolean;
    remove?: boolean;
    soldOut: boolean;
+   pair: string[];
+   sold: number;
 }
 
 const ProductInCart = ({
+   id,
    image,
    stars,
    title,
@@ -37,7 +46,34 @@ const ProductInCart = ({
    reviews,
    soldOut,
    buttonPrice,
+   pair,
+   sold,
 }: ProductInCartProps) => {
+   const [load, setLoad] = useState(false);
+
+   const dispatch = useAppDispatch();
+
+   const clickHandler = () => {
+      setLoad(true);
+      setTimeout(() => {
+         setLoad(false);
+         dispatch(
+            addCart({
+               id: id,
+               title: title,
+               image: image,
+               price: buttonPrice!,
+               reviews: reviews,
+               totalPrice: buttonPrice!,
+               soldOut: soldOut,
+               quantity: 1,
+               sold: sold,
+               pair: pair,
+            })
+         );
+      }, 500);
+   };
+
    return (
       <div className="bg-white flex gap-2 flex-between h-full rounded-[8px] p-[16px] relative">
          <div className="flex gap-3 mr-[150px] items-center">
@@ -65,14 +101,17 @@ const ProductInCart = ({
             <CountProduct className="absolute right-[20px] bottom-[20px]" />
          )}
          {button && (
-            <Button
-               soldOut={soldOut}
-               text={`add - $${buttonPrice}`}
-               size={'sm'}
-               className="text-[10px] uppercase absolute right-[20px] bottom-[20px] bg-gradient-to-r from-pink-400 to-pink-600 "
-            />
+            <div className="absolute right-[20px] bottom-[20px]">
+               <Button
+                  soldOut={soldOut}
+                  load={load}
+                  text={`add - $${buttonPrice}`}
+                  size={'sm'}
+                  className="text-[10px] uppercase  bg-gradient-to-r from-pink-400 to-pink-600 "
+                  onClick={clickHandler}
+               />
+            </div>
          )}
-
          {remove && (
             <div className="bg-white absolute left-[-10px] top-[-10px] rounded-full p-2">
                <AiOutlineClose className="text-[14px] cursor-pointer " />
