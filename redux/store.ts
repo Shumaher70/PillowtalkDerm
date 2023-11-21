7489a39995fab5ccdb42bd46a40ac3b5f3386526
oleg <olegvraby@gmail.com>
2023-11-21 17:21:19 +0200

    add:store: add the middleware localStorage

--- redux/store.ts
+++ redux/store.ts
@@ -1,9 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './features/cartSlice';
+import cartStorage from './localStorages/cartStorage';
 export const store = configureStore({
    reducer: {
       cartReducer,
    },
+   preloadedState: cartStorage().getLocalStorageCart(),
+   middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().prepend(
+         cartStorage().localStorageCartMiddleware.middleware
+      ),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
