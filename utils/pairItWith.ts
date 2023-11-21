8f0c6913088597d24a230a7d173e22d5d5a4689d
oleg <olegvraby@gmail.com>
2023-11-21 10:47:29 +0200

    add:pairItWith: add logic to remove the cart from the pair with the carousel, if it is in the cart

--- utils/pairItWith.ts
+++ utils/pairItWith.ts
@@ -3,17 +3,22 @@ import { CartType, ProductType } from '@/types';
 const pairItWith = (carts: CartType[], products: ProductType[]) => {
    const pairs = carts.map((item) => item.pair).flatMap((pair) => pair);
 
-   let pairItwithArr: ProductType[] = [];
+   let pairItWithArr: ProductType[] = [];
 
    for (let i = 0; i < products.length; i++) {
       for (let j = 0; j < pairs.length; j++) {
          if (products[i].id === pairs[j]) {
-            pairItwithArr.push(products[i]);
+            pairItWithArr.push(products[i]);
          }
       }
    }
 
-   return pairItwithArr;
+   const cartIp = Array.from(new Set(carts.map((obj) => obj.id)));
+   pairItWithArr = Array.from(
+      new Set(pairItWithArr.filter((obj) => !cartIp.includes(obj.id)))
+   );
+
+   return pairItWithArr;
 };
 
 export default pairItWith;
