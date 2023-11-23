4532ed525b2843e16295e388fa30fe2b52117be9
oleg <olegvraby@gmail.com>
2023-11-23 11:22:29 +0200

    change:CarouselCart: remove the ProductInCard and the CartInCarousel

--- app/components/navbar/components/cart/components/carouselCart/CarouselCart.tsx
+++ app/components/navbar/components/cart/components/carouselCart/CarouselCart.tsx
@@ -1,26 +1,25 @@
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 
-import ProductInCart from '../productInCart/ProductInCart';
-
 import { CartType, ProductType } from '@/types';
 import bestSellers from '@/utils/bestSellers';
 import pairItWith from '@/utils/pairItWith';
+import CartInCarousel from './CartInCarousel';
 
-const CarouselCart = ({
-   carts,
-   products,
-}: {
+interface CarouselCartProps {
    carts: CartType[];
    products: ProductType[];
-}) => {
+}
+
+const CarouselCart = ({ carts, products }: CarouselCartProps) => {
    const responsive = {
       desktop: {
          breakpoint: { max: 3000, min: 0 },
-         items: 1.07,
+         items: 1,
          slidesToSlide: 1,
       },
    };
+
    const CustomDot = ({
       onClick,
       active,
@@ -32,72 +31,103 @@ const CarouselCart = ({
          <button
             className={`${
                active
-                  ? 'bg-gradient-to-t from-pink-400 to-pink-600'
+                  ? 'bg-gradient-to-t from-pink-400 to-pink-600 border-pink-600'
                   : 'inactive'
-            } w-[8px] h-[8px] border-1 border-[#F32E79] rounded-full m-1`}
+            } w-[8px] h-[8px] border-1 border-purple-300 rounded-full m-1 mr-3`}
             onClick={() => onClick()}
          ></button>
       );
    };
 
    return (
-      <Carousel
-         swipeable={true}
-         arrows={false}
-         draggable={true}
-         showDots={true}
-         responsive={responsive}
-         ssr={true}
-         infinite={false}
-         keyBoardControl={true}
-         transitionDuration={10}
-         containerClass="carousel-container"
-         dotListClass="block w-max absolute -top-[19px] left-[100%] -translate-x-full"
-         itemClass="p-2"
-         className="overflow-visible"
-         customDot={
-            pairItWith(carts, products).length > 1 ||
-            pairItWith(carts, products).length === 0 ? (
-               <CustomDot onClick={() => {}} active={false} />
-            ) : (
-               <></>
-            )
-         }
-      >
-         {pairItWith(carts, products).length > 0
-            ? pairItWith(carts, products).map((cart) => (
-                 <ProductInCart
-                    key={cart.id}
-                    image={cart.images[0]}
-                    title={cart.title}
-                    reviews={cart.reviews}
-                    soldOut={cart.soldOut}
-                    stars
-                    readme="More details"
-                    button
-                    buttonPrice={cart.price}
-                    pair={cart.pair}
-                    sold={cart.sold}
-                    id={cart.id}
-                 />
-              ))
-            : bestSellers(products).map((product) => (
-                 <ProductInCart
-                    key={product.id}
-                    image={product.images[0]}
-                    title={product.title}
-                    reviews={product.reviews}
-                    soldOut={product.soldOut}
-                    stars
-                    readme="More details"
-                    button
-                    buttonPrice={product.price}
-                    pair={product.pair}
-                    sold={product.sold}
-                    id={product.id}
-                 />
-              ))}
-      </Carousel>
+      <>
+         {pairItWith(carts, products).length > 0 && (
+            <Carousel
+               swipeable={true}
+               arrows={false}
+               draggable={true}
+               showDots={true}
+               responsive={responsive}
+               ssr={true}
+               infinite={false}
+               keyBoardControl={true}
+               transitionDuration={10}
+               containerClass="carousel-container"
+               dotListClass="block w-max h-[10px] absolute -top-[19px] left-[100%] -translate-x-full z-[0]"
+               itemClass="p-2"
+               className="overflow-visible select-none"
+               customDot={
+                  pairItWith(carts, products).length > 1 ||
+                  pairItWith(carts, products).length === 0 ? (
+                     <CustomDot onClick={() => {}} active={false} />
+                  ) : (
+                     <></>
+                  )
+               }
+            >
+               {pairItWith(carts, products).length > 0 &&
+                  pairItWith(carts, products).map((cart) => (
+                     <CartInCarousel
+                        key={cart.id}
+                        image={cart.images[0]}
+                        title={cart.title}
+                        reviews={cart.reviews}
+                        soldOut={cart.soldOut}
+                        stars
+                        readme="More details"
+                        button
+                        price={cart.price}
+                        pair={cart.pair}
+                        sold={cart.sold}
+                        id={cart.id}
+                     />
+                  ))}
+            </Carousel>
+         )}
+
+         {pairItWith(carts, products).length === 0 && (
+            <Carousel
+               swipeable={true}
+               arrows={false}
+               draggable={true}
+               showDots={true}
+               responsive={responsive}
+               ssr={true}
+               infinite={false}
+               keyBoardControl={true}
+               transitionDuration={10}
+               containerClass="carousel-container"
+               dotListClass="block w-max h-[10px] absolute -top-[19px] left-[100%] -translate-x-full z-[0]"
+               itemClass="p-2"
+               className="overflow-visible select-none"
+               customDot={
+                  pairItWith(carts, products).length > 1 ||
+                  pairItWith(carts, products).length === 0 ? (
+                     <CustomDot onClick={() => {}} active={false} />
+                  ) : (
+                     <></>
+                  )
+               }
+            >
+               {bestSellers(products).map((product) => (
+                  <CartInCarousel
+                     key={product.id}
+                     image={product.images[0]}
+                     title={product.title}
+                     reviews={product.reviews}
+                     soldOut={product.soldOut}
+                     stars
+                     readme="More details"
+                     button
+                     price={product.price}
+                     pair={product.pair}
+                     sold={product.sold}
+                     id={product.id}
+                  />
+               ))}
+            </Carousel>
+         )}
+      </>
    );
 };
 
