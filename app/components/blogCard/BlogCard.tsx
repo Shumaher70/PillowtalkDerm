2391c035a1448439aac37b1650efbf9a25839728
oleg <olegvraby@gmail.com>
2023-11-27 18:56:57 +0200

    change:BlogCard: the markup

--- app/components/blogCard/BlogCard.tsx
+++ app/components/blogCard/BlogCard.tsx
@@ -1,34 +1,31 @@
-import Link from 'next/link';
 import ImageCard from '../card/components/ImageCard';
 import TitleCard from '../card/components/TitleCard';
 import ReadMe from '../card/components/ReadMe';
 
 interface BlogCardProps {
-   blog: {
-      id: string;
-      images: string[];
-      title: string;
-   };
+   id: string;
+   images: string[];
+   title: string;
    extra?: string;
 }
 
-const BlogCard = ({ blog, extra }: BlogCardProps) => {
-   return (
-      <div className="bg-white rounded-[8px] m-2">
-         <div className="m-[10px] rounded-[5px]">
-            <ImageCard image={blog.images[0]} title={blog.title} />
-            <div className="pt-[8px] text-center">
-               <TitleCard title={blog.title} />
+const BlogCard = ({ id, images, title, extra }: BlogCardProps) => {
+   if (images)
+      return (
+         <div className="bg-white rounded-[8px] m-2 flex flex-col justify-between">
+            <div className="m-[10px] rounded-[5px] ">
+               <ImageCard image={images[0]} title={title} />
+               <div className="pt-[8px] text-center">
+                  <TitleCard title={title} />
+               </div>
             </div>
-
             {extra && (
-               <div className="text-center h-full">
+               <div className="text-center pb-5">
                   <ReadMe text={extra} src="" />
                </div>
             )}
          </div>
-      </div>
-   );
+      );
 };
 
 export default BlogCard;
