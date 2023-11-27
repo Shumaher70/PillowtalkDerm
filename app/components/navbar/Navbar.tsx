d3b7e2211feef515785018e324b3279bc52d5c65
oleg <olegvraby@gmail.com>
2023-11-27 18:58:40 +0200

    change:Navbar: remove getPrisma and add reactQuery

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,82 +1,89 @@
+'use client';
+
+import { useQueries } from '@tanstack/react-query';
+
 import Burger from './components/burger/Burger';
 import Search from './components/search/Search';
 import Cart from './components/cart/Cart';
-import Info from './components/Info';
+import Info from './components/info/Info';
 import Login from './components/Login';
 import Logo from './components/Logo';
-import { prisma } from '@/lib/prisma';
-import { BlogType, ProductType } from '@/types';
 
-const Navbar = async () => {
-   const products: ProductType[] = await prisma.product.findMany({
-      select: {
-         id: true,
-         images: true,
-         title: true,
-         price: true,
-         reviews: true,
-         soldOut: true,
-         sold: true,
-         pair: true,
-      },
-   });
+const getProduct = async () => {
+   const data = await fetch('http://localhost:3000/api/products');
+   const product = await data.json();
+   return product;
+};
 
-   const blogs: BlogType[] = await prisma.blog.findMany({
-      select: {
-         id: true,
-         images: true,
-         title: true,
-      },
+const getBlog = async () => {
+   const data = await fetch('http://localhost:3000/api/blog');
+   const blog = await data.json();
+   return blog;
+};
+
+const Navbar = () => {
+   const [products, blogs] = useQueries({
+      queries: [
+         { queryKey: ['product'], queryFn: getProduct },
+         {
+            queryKey: ['blog'],
+            queryFn: getBlog,
+         },
+      ],
    });
 
    return (
-      <header
-         className="
-         w-full
-         md:h-[88px] 
-         md:px-[40px]  
-         lg:px-[80px] 
-         px-[16px] 
-         py-[8px] 
-         flex 
-         flex-center 
-         rounded-b-[10px] 
-         lg:rounded-b-[30px] 
-         bg-accent
-         fixed 
-         top-0
+      <>
+         <header
+            className="
+            w-full
+            md:px-[40px]  
+            lg:px-[80px] 
+            px-[16px] 
+            py-[8px] 
+            rounded-b-[10px] 
+            lg:rounded-b-[30px] 
+            bg-accent
+            fixed 
+            top-0
          "
-      >
-         <nav className="flex flex-between w-full">
-            <div className="flex justify-start gap-3 w-full">
-               <Info className="hidden lg:flex" />
-               <Burger
-                  className="lg:hidden"
-                  products={products}
-                  blogs={blogs}
-               />
-               <Search
-                  className="block lg:hidden"
-                  products={products}
-                  blogs={blogs}
-               />
-            </div>
+         >
+            <nav className="flex flex-between w-full">
+               <div className="flex justify-start gap-3 w-full">
+                  <Info className="hidden lg:flex" />
+                  <Burger
+                     className="lg:hidden"
+                     products={products.data}
+                     blogs={blogs.data}
+                  />
+                  <Search
+                     className="block lg:hidden"
+                     products={products.data}
+                     blogs={blogs.data}
+                  />
+               </div>
 
-            <div className="flex flex-center w-full">
-               <Logo />
-            </div>
+               <div className="flex flex-center w-full">
+                  <Logo />
+               </div>
 
-            <div className="flex justify-end items-center gap-3 w-full">
-               <Search
-                  className="hidden lg:block"
-                  products={products}
-                  blogs={blogs}
-               />
-               <Login />
-               <Cart />
+               <div className="flex justify-end items-center gap-3 w-full">
+                  <Search
+                     className="hidden lg:block"
+                     products={products.data}
+                     blogs={blogs.data}
+                  />
+                  <Login />
+                  <Cart />
+               </div>
+            </nav>
+
+            <div>
+               {/* <Shop products={products} /> */}
+               {/* <SkinNerdAcademy blogs={blogs} /> */}
             </div>
-         </nav>
-      </header>
+         </header>
+      </>
    );
 };
 
