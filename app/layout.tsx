8fa23a75e1368382a1550ad6a0d500dd34153dfe
oleg <olegvraby@gmail.com>
2023-11-29 12:04:56 +0200

    add:layout:footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 
 import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
 
 import localFont from 'next/font/local';
 import QueryProvider from './QueryProvider';
@@ -24,6 +25,7 @@ export default function RootLayout({
                <Providers>
                   <Navbar />
                   {children}
+                  <Footer />
                </Providers>
             </QueryProvider>
          </body>
