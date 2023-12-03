8914c0c629b093435f0468d39c70a3f60a887971
oleg <olegvraby@gmail.com>
2023-12-03 14:11:19 +0200

    change:motionDiv: remove  the custom component and add  the simple import

--- motion/motionDiv.tsx
+++ motion/motionDiv.tsx
@@ -1,10 +1,4 @@
 'use client';
 import { motion } from 'framer-motion';
 
-import React from 'react';
-
-const motionDiv = ({ children }: { children: React.ReactNode }) => {
-   return <motion.div>{children}</motion.div>;
-};
-
-export default motionDiv;
+export const MotionDiv = motion.div;
