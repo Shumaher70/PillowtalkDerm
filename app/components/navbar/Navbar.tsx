6445a8dadc8bf3cbf59c14e3a8a860a2fe063c41
oleg <olegvraby@gmail.com>
2023-12-03 14:04:04 +0200

    change:Navbar: remove a motion action from component and add the searchMobile instead of Search

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,21 +1,24 @@
 'use client';
 
-import { useQueries, useSuspenseInfiniteQuery } from '@tanstack/react-query';
+import { useQueries } from '@tanstack/react-query';
 import { motion } from 'framer-motion';
 
-import { useAppSelector } from '@/redux/hooks';
-import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 
 import Burger from './components/burger/Burger';
-import Search from './components/search/Search';
+import SearchMobile from './components/search/SearchMobile';
 import Cart from './components/cart/Cart';
 import Info from './components/info/Info';
 import Login from './components/Login';
 import Logo from './components/Logo';
-import Shop from './components/info/components/Shop';
-import SkinNerdAcademy from './components/info/components/SkinNerdAcademy';
-import { slideShop, slideSkinNerdAcademy } from '@/redux/features/sidebarSlice';
+
+import {
+   slideSearch,
+   slideShop,
+   slideSkinNerdAcademy,
+} from '@/redux/features/sidebarSlice';
+
+import { CiSearch } from 'react-icons/ci';
 
 const getProduct = async () => {
    const data = await fetch('http://localhost:3000/api/products');
@@ -30,25 +33,7 @@ const getBlog = async () => {
 };
 
 const Navbar = () => {
-   const slideSlice = useAppSelector((state) => state.sidebarReducer);
    const dispatch = useDispatch();
-
-   const [screen, setScreen] = useState(false);
-
-   useEffect(() => {
-      const screenWidth = () => {
-         if (window.innerWidth > 768) {
-            setScreen(true);
-         } else {
-            setScreen(false);
-         }
-      };
-      screenWidth();
-
-      window.addEventListener('resize', screenWidth);
-
-      return () => window.removeEventListener('resize', screenWidth);
-   }, []);
 
    const [products, blogs] = useQueries({
       queries: [
@@ -60,48 +45,6 @@ const Navbar = () => {
       ],
    });
 
-   const displayShop = {
-      display: {
-         transition: { duration: 0.3 },
-         opacity: slideSlice.slideShop ? 1 : 0,
-         display: slideSlice.slideShop ? 'block' : 'none',
-      },
-   };
-
-   const displaySkin = {
-      display: {
-         transition: { duration: 0.3 },
-         opacity: slideSlice.slideSkinNerdAcademy ? 1 : 0,
-         display: slideSlice.slideSkinNerdAcademy ? 'block' : 'none',
-      },
-   };
-
-   const displayHeight = {
-      shop: {
-         transition: { duration: 0.3 },
-         height: slideSlice.slideShop ? 440 : screen ? 88 : 45,
-      },
-      skin: {
-         transition: { duration: 0.3 },
-         height: slideSlice.slideSkinNerdAcademy ? 540 : screen ? 88 : 45,
-      },
-
-      close: {
-         transition: { duration: 0.3 },
-         height: screen ? 88 : 45,
-      },
-   };
-
-   const height = () => {
-      if (slideSlice.slideShop) {
-         return 'shop';
-      } else if (slideSlice.slideSkinNerdAcademy) {
-         return 'skin';
-      } else {
-         return 'close';
-      }
-   };
-
    return (
       <>
          {products.isSuccess && blogs.isSuccess && (
@@ -110,9 +53,6 @@ const Navbar = () => {
                   dispatch(slideShop(false));
                   dispatch(slideSkinNerdAcademy(false));
                }}
-               variants={displayHeight}
-               initial={{ height: `${screen ? '88px' : '35px'}` }}
-               animate={height()}
                className={`
                w-full
                container-px
@@ -124,7 +64,7 @@ const Navbar = () => {
                overflow-hidden
                `}
             >
-               <nav className="flex flex-between  md:h-[72px] w-full">
+               <nav className="flex flex-between md:h-[72px] w-full">
                   <div className="flex justify-start gap-3 w-full">
                      <Info className="hidden lg:flex" />
                      <Burger
@@ -132,7 +72,7 @@ const Navbar = () => {
                         products={products.data}
                         blogs={blogs.data}
                      />
-                     <Search
+                     <SearchMobile
                         className="block lg:hidden"
                         products={products.data}
                         blogs={blogs.data}
@@ -144,25 +84,20 @@ const Navbar = () => {
                   </div>
 
                   <div className="flex justify-end items-center gap-3 w-full">
-                     <Search
-                        className="hidden lg:block"
-                        products={products.data}
-                        blogs={blogs.data}
-                     />
+                     <div className="hidden lg:block">
+                        <CiSearch
+                           className={`text-[30px] cursor-pointer `}
+                           onClick={() => {
+                              dispatch(slideSearch(true));
+                              dispatch(slideShop(false));
+                              dispatch(slideSkinNerdAcademy(false));
+                           }}
+                        />
+                     </div>
                      <Login />
                      <Cart />
                   </div>
                </nav>
-
-               <div>
-                  <motion.div variants={displayShop} animate="display">
-                     <Shop products={products.data} />
-                  </motion.div>
-
-                  <motion.div variants={displaySkin} animate="display">
-                     <SkinNerdAcademy blogs={blogs.data} />
-                  </motion.div>
-               </div>
             </motion.header>
          )}
       </>
