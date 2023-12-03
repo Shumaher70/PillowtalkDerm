b07a28bad7b1236c69838e06815604fcb468911c
oleg <olegvraby@gmail.com>
2023-12-03 14:08:45 +0200

    add:sidebarSlice: sidebarBurger&sidebarSearch

--- redux/features/sidebarSlice.ts
+++ redux/features/sidebarSlice.ts
@@ -2,14 +2,22 @@ import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 
 interface sidebar {
    sidebarCart: boolean;
+   sidebarBurger: boolean;
+   sidebarSearch: boolean;
+
    slideShop: boolean;
    slideSkinNerdAcademy: boolean;
+   slideSearch: boolean;
 }
 
 const initialState = {
    sidebarCart: false,
+   sidebarBurger: false,
+   sidebarSearch: false,
+
    slideShop: false,
    slideSkinNerdAcademy: false,
+   slideSearch: false,
 } as sidebar;
 
 export const sidebar = createSlice({
@@ -19,15 +27,33 @@ export const sidebar = createSlice({
       sidebarCart: (state, action: PayloadAction<boolean>) => {
          state.sidebarCart = action.payload;
       },
+      sidebarBurger: (state, action: PayloadAction<boolean>) => {
+         state.sidebarBurger = action.payload;
+      },
+      sidebarSearch: (state, action: PayloadAction<boolean>) => {
+         state.sidebarSearch = action.payload;
+      },
+
       slideShop: (state, action: PayloadAction<boolean>) => {
          state.slideShop = action.payload;
       },
       slideSkinNerdAcademy: (state, action: PayloadAction<boolean>) => {
          state.slideSkinNerdAcademy = action.payload;
       },
+      slideSearch: (state, action: PayloadAction<boolean>) => {
+         state.slideSearch = action.payload;
+      },
    },
 });
 
-export const { sidebarCart, slideShop, slideSkinNerdAcademy } = sidebar.actions;
+export const {
+   sidebarCart,
+   slideShop,
+   slideSkinNerdAcademy,
+
+   sidebarBurger,
+   sidebarSearch,
+   slideSearch,
+} = sidebar.actions;
 
 export default sidebar.reducer;
