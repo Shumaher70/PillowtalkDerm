5c44ab8381545e5a6b915c103974db8ee5a84b8e
oleg <olegvraby@gmail.com>
2023-12-05 10:55:21 +0200

    change:AwardWinning: bg&screen&padding

--- app/components/card/components/AwardWinning.tsx
+++ app/components/card/components/AwardWinning.tsx
@@ -1,6 +1,6 @@
 const AwardWinning = () => {
    return (
-      <p className="uppercase bg-accent px-[16px] py-[4px] text-[#6A1BA6] rounded-full">
+      <p className="uppercase bg-white shadow-md md:text-[16px] text-[11px] md:px-[16px] p-[8px] md:py-[4px] py-[2px] text-[#6A1BA6] rounded-full">
          Award-Winning
       </p>
    );
