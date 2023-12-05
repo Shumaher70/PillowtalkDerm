298b5576799079e36b4fafbdbfa322d8b4f6b774
oleg <olegvraby@gmail.com>
2023-12-05 10:57:03 +0200

    fix:Stars: screen&size

--- app/components/card/components/Stars.tsx
+++ app/components/card/components/Stars.tsx
@@ -9,25 +9,32 @@ const Stars = ({ stars }: StarsProps) => {
    for (let i = 0; i < 5; i++) {
       const difference = parseFloat((stars - i).toFixed(1));
       if (difference >= 1)
-         starts.push(<IoMdStar className="text-pink-700 w-5 h-5" />);
+         starts.push(
+            <IoMdStar className="text-pink-700 md:w-5 md:h-5 w-3 h-3" />
+         );
       else if (difference < 1 && difference > 0) {
          if (difference <= 0.2)
-            starts.push(<IoIosStarOutline className="text-pink-700 w-5 h-5" />);
+            starts.push(
+               <IoIosStarOutline className="text-pink-700 md:w-5 md:h-5 w-3 h-3" />
+            );
          else if (difference > 0.2 && difference <= 0.6)
-            starts.push(<IoIosStarHalf className="text-pink-700 w-5 h-5" />);
-         else starts.push(<IoMdStar className="text-pink-700 w-5 h-5" />);
+            starts.push(
+               <IoIosStarHalf className="text-pink-700 md:w-5 md:h-5 w-3 h-3" />
+            );
+         else
+            starts.push(
+               <IoMdStar className="text-pink-700 md:w-5 md:h-5 w-3 h-3" />
+            );
       } else
-         starts.push(<IoIosStarOutline className="text-pink-700 w-5 h-5" />);
+         starts.push(
+            <IoIosStarOutline className="text-pink-700 md:w-5 md:h-5 w-3 h-3" />
+         );
    }
 
    return (
       <div className="flex gap-1">
          {starts.map((star, index) => {
-            return (
-               <div key={index} className="div">
-                  {star}
-               </div>
-            );
+            return <div key={index}>{star}</div>;
          })}
       </div>
    );
