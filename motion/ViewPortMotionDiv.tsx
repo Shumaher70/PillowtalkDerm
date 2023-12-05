6cc1781b21aefbc9eb1da012a1da32fcce4ff1cc
oleg <olegvraby@gmail.com>
2023-12-05 12:02:20 +0200

    create:ViewPortMotionDiv&ViewPortMotionDivArr

---
+++ motion/ViewPortMotionDiv.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { MotionDiv } from './MotionDiv';
+
+const ViewportMotionDiv = ({ children }: { children: React.ReactNode }) => {
+   const container = {
+      hidden: { opacity: 0, y: 100 },
+      visible: {
+         opacity: 1,
+         y: 0,
+         transition: {
+            delayChildren: 0.3,
+            staggerChildren: 0.2,
+         },
+      },
+   };
+
+   return (
+      <MotionDiv
+         variants={container}
+         initial={'hidden'}
+         whileInView={'visible'}
+         viewport={{ once: true }}
+      >
+         {children}
+      </MotionDiv>
+   );
+};
+
+export default ViewportMotionDiv;
---
+++ motion/ViewPortMotionDivArr.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { MotionDiv } from './MotionDiv';
+
+const ViewportMotionDivArr = ({ children }: { children: React.ReactNode }) => {
+   const item = {
+      hidden: { y: 20, opacity: 0 },
+      visible: {
+         y: 0,
+         opacity: 1,
+      },
+   };
+
+   return (
+      <MotionDiv variants={item} viewport={{ once: true }}>
+         {children}
+      </MotionDiv>
+   );
+};
+
+export default ViewportMotionDivArr;
