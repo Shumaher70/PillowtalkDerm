4fca073dcba7d80a649017c30bc47dda6e5f3387
oleg <olegvraby@gmail.com>
2023-12-06 19:18:30 +0200

    add:TitleCard: prop ClassName

--- app/components/card/components/TitleCard.tsx
+++ app/components/card/components/TitleCard.tsx
@@ -1,15 +1,21 @@
-const TitleCard = ({ title }: { title: string }) => {
+const TitleCard = ({
+   title,
+   className,
+}: {
+   title: string;
+   className?: string;
+}) => {
    return (
       <p
-         className="
-            text-black 
+         className={`
+         text-black 
             md:text-[16px]
             text-[13px]
             font-[500] 
             font-sans
-            capitalize 
-            leading-[16px]	
-          "
+            leading-[16px]
+            ${className}
+         `}
       >
          {title}
       </p>
