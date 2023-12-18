eda7b7b131830b7784acf87f0fcdf8ee2772e29c
oleg <olegvraby@gmail.com>
2023-12-18 13:28:18 +0200

    change:OverFlow: The logic has not changed

--- app/Overflow.tsx
+++ app/Overflow.tsx
@@ -1,14 +1,14 @@
-'use client';
-import { MotionDiv } from '@/motion/MotionDiv';
-import { useAppSelector } from '@/redux/hooks';
-import React, { useEffect, useState } from 'react';
+"use client"
+import { MotionDiv } from "@/motion/MotionDiv"
+import { useAppSelector } from "@/redux/hooks"
+import React, { useEffect, useState } from "react"
 
 const Overflow = () => {
-   const [overflow, setOverflow] = useState(true);
-   const sidebarSlice = useAppSelector((state) => state.sidebarReducer);
+   const [overflow, setOverflow] = useState(true)
+   const sidebarSlice = useAppSelector((state) => state.sidebarReducer)
 
    useEffect(() => {
-      if (typeof document !== 'undefined')
+      if (typeof document !== "undefined")
          if (
             sidebarSlice.sidebarBurger ||
             sidebarSlice.sidebarCart ||
@@ -17,12 +17,12 @@ const Overflow = () => {
             sidebarSlice.slideShop ||
             sidebarSlice.slideSkinNerdAcademy
          ) {
-            setOverflow(false);
+            setOverflow(false)
          } else {
-            setOverflow(true);
+            setOverflow(true)
          }
 
-      document.body.style.overflow = overflow ? 'visible' : 'hidden';
+      document.body.style.overflow = overflow ? "visible" : "hidden"
    }, [
       overflow,
       sidebarSlice.sidebarBurger,
@@ -31,16 +31,16 @@ const Overflow = () => {
       sidebarSlice.slideSearch,
       sidebarSlice.slideShop,
       sidebarSlice.slideSkinNerdAcademy,
-   ]);
+   ])
 
    return (
       <MotionDiv
-         initial={{ y: '-100%' }}
+         initial={{ y: "-100%" }}
          transition={{ duration: 0 }}
-         animate={{ y: `${overflow ? '-100%' : 0}` }}
-         className="w-full bg-black/30 h-full absolute top-0"
+         animate={{ y: `${overflow ? "-100%" : 0}` }}
+         className="fixed top-0 z-[5] h-full w-full bg-black/30"
       ></MotionDiv>
-   );
-};
+   )
+}
 
-export default Overflow;
+export default Overflow
