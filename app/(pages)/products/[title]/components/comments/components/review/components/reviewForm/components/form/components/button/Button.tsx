945947466769f802525e29aebb583aaa5fc09b38
oleg <olegvraby@gmail.com>
2023-12-29 13:04:48 +0200

    add:products/comment/Button:className props

--- app/(pages)/products/[title]/components/comments/components/review/components/reviewForm/components/form/components/button/Button.tsx
+++ app/(pages)/products/[title]/components/comments/components/review/components/reviewForm/components/form/components/button/Button.tsx
@@ -1,14 +1,16 @@
 const Button = ({
    children,
    onClick,
+   className,
 }: {
    children: React.ReactNode
    onClick?: () => void
+   className?: string
 }) => {
    return (
       <button
          onClick={onClick}
-         className="mt-5 cursor-pointer rounded-full bg-purple-700 px-[16px] py-[8px] uppercase text-white"
+         className={`mt-5 cursor-pointer rounded-full bg-purple-700 px-[16px] py-[8px] uppercase text-white ${className}`}
       >
          {children}
       </button>
