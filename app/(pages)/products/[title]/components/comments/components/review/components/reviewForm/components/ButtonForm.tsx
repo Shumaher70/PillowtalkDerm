bd3aaf895124aa6032a0dbf4f33ab5f114ee8517
oleg <olegvraby@gmail.com>
2024-01-04 13:16:07 +0200

    add:products/comments/review/buttonForm:add condition for check sign-in

--- app/(pages)/products/[title]/components/comments/components/review/components/reviewForm/components/ButtonForm.tsx
+++ app/(pages)/products/[title]/components/comments/components/review/components/reviewForm/components/ButtonForm.tsx
@@ -2,14 +2,24 @@
 
 import { useAppDispatch } from "@/redux/hooks"
 import { reviewForm } from "@/redux/features/sidebarSlice"
+import { useRouter } from "next/navigation"
+import { useUser } from "@clerk/nextjs"
 
 const ButtonForm = () => {
    const dispatch = useAppDispatch()
+   const router = useRouter()
+   const { isSignedIn } = useUser()
 
    return (
       <button
          className="text-p w-full max-w-[600px] rounded-full bg-purple-700 px-[16px] py-[8px] uppercase text-white active:bg-purple-700/90 lg:w-[200px]"
-         onClick={() => dispatch(reviewForm(true))}
+         onClick={() => {
+            if (isSignedIn) {
+               dispatch(reviewForm(true))
+            } else {
+               router.push("/sign-in")
+            }
+         }}
       >
          write a review
       </button>
