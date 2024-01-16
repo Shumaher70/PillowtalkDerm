1788e4dba2b7f3d7aa944b25df66157a070d48e5
oleg <olegvraby@gmail.com>
2024-01-16 19:08:06 +0200

    add:products/Customer: the Verified component

--- app/(pages)/products/[title]/components/comments/components/comment/components/user/components/Customer.tsx
+++ app/(pages)/products/[title]/components/comments/components/comment/components/user/components/Customer.tsx
@@ -1,19 +1,13 @@
-import { prisma } from "@/lib/prisma"
 import { IoCheckmarkCircle } from "react-icons/io5"
+import Verified from "./Verified"
 
-const Customer = async ({
-   name: nameProps,
-   email,
-}: {
+interface CustomerProps {
    name: string
    email: string
-}) => {
-   const emailUsers = await prisma.user.findUnique({
-      where: {
-         email: email,
-      },
-   })
+   verified: boolean
+}
 
+const Customer = ({ name: nameProps, email, verified }: CustomerProps) => {
    const nameIcon = (name: string) => {
       if (name.split(" ").length === 1) {
          const first = name.slice(0, 1).toUpperCase()
@@ -55,21 +49,22 @@ const Customer = async ({
    }
 
    return (
-      <div className="flex w-full items-center gap-2">
+      <div className="items flex w-full gap-2">
          <div className="flex-center flex h-[60px] w-[60px] rounded-full bg-black text-[30px] uppercase text-white">
             {nameIcon(nameProps)}
          </div>
 
          <div className="flex flex-col">
             <span className="text-[16px] capitalize">{name(nameProps)}</span>
-
-            {emailUsers?.img && (
+            {verified && (
                <div className="flex items-center gap-[2px]">
                   <IoCheckmarkCircle className="text-[20px] text-[#F92672]" />
                   <span className="text-[16px]">Verified Buyer</span>
                </div>
             )}
          </div>
+
+         <Verified email={email} />
       </div>
    )
 }
