812f8115cf5dae1bb00d73d20b1b818d4e05aebf
oleg <olegvraby@gmail.com>
2024-01-16 19:13:28 +0200

    change:products/comment/Comment: the type interface has been changed and the action of sending a comment when clicking on an image has been added

--- app/(pages)/products/[title]/components/comments/components/comment/components/userComment/components/Comment.tsx
+++ app/(pages)/products/[title]/components/comments/components/comment/components/userComment/components/Comment.tsx
@@ -1,14 +1,16 @@
 "use client"
+import { useAppDispatch } from "@/redux/hooks"
 import Image from "next/image"
 import { useState } from "react"
+import { commentAction } from "@/redux/features/commentImagesSlice"
+import { Review } from "@prisma/client"
 
 interface CommentProps {
-   title: string
-   comment: string
-   media: string[]
+   comment: Review
 }
 
-const Comment = ({ title, comment, media }: CommentProps) => {
+const Comment = ({ comment }: CommentProps) => {
+   const dispatch = useAppDispatch()
    const [extraTitleHeight, setExtraTitleHeight] = useState(false)
    const [extraCommentHeight, setExtraCommentHeight] = useState(false)
 
@@ -19,9 +21,9 @@ const Comment = ({ title, comment, media }: CommentProps) => {
                text-[20px] font-bold`}
          >
             <>
-               {!extraTitleHeight && title.trim().slice(0, 70)}
-               {extraTitleHeight && title}
-               {title.length >= 70 && !extraTitleHeight && (
+               {!extraTitleHeight && comment.title.trim().slice(0, 70)}
+               {extraTitleHeight && comment.title}
+               {comment.title.length >= 70 && !extraTitleHeight && (
                   <span
                      onClick={() => setExtraTitleHeight(true)}
                      className="inline-block cursor-pointer text-[20px] font-bold text-[#F92672]"
@@ -34,16 +36,38 @@ const Comment = ({ title, comment, media }: CommentProps) => {
 
          {extraCommentHeight && (
             <>
-               <p className="text-p">{comment}</p>
-               {media.length > 0 && (
+               <p className="text-p">{comment.comment}</p>
+               {comment.images.length > 0 && (
                   <div className="flex w-full flex-wrap gap-2">
-                     {media?.map((image) => (
-                        <div key={image}>
+                     {comment.images?.map((image, index) => (
+                        <div
+                           key={image}
+                           onClick={() => {
+                              dispatch(
+                                 commentAction({
+                                    recommend: comment.recommend,
+                                    createdAt: comment.createdAt,
+                                    titleReview: comment.title,
+                                    verified: comment.verified,
+                                    dislike: comment.dislike,
+                                    imageUrl: comment.images,
+                                    review: comment.comment,
+                                    stars: comment.rating,
+                                    email: comment.email,
+                                    commentImages: true,
+                                    name: comment.name,
+                                    indexImage: index,
+                                    like: comment.like,
+                                    id: comment.id,
+                                 })
+                              )
+                           }}
+                        >
                            <Image
                               width={100}
                               height={100}
                               alt="comment image"
-                              className="h-[75px] w-[75px] cursor-pointer object-cover"
+                              className="h-[100px] w-[100px] cursor-pointer object-cover"
                               src={image}
                            />
                         </div>
@@ -56,9 +80,9 @@ const Comment = ({ title, comment, media }: CommentProps) => {
          {!extraCommentHeight && (
             <>
                <p className="text-p">
-                  {comment.slice(0, 400)}
+                  {comment.comment.slice(0, 400)}
 
-                  {title.length >= 400 && !extraCommentHeight && (
+                  {comment.title.length >= 400 && !extraCommentHeight && (
                      <span
                         onClick={() => setExtraCommentHeight(true)}
                         className="inline-block cursor-pointer text-[16px] font-bold text-[#F92672]"
@@ -67,15 +91,37 @@ const Comment = ({ title, comment, media }: CommentProps) => {
                      </span>
                   )}
                </p>
-               {media.length > 0 && (
+               {comment.images.length > 0 && (
                   <div className="flex w-full flex-wrap gap-2">
-                     {media?.map((image) => (
-                        <div key={image}>
+                     {comment.images?.map((image, index) => (
+                        <div
+                           key={image}
+                           onClick={() => {
+                              dispatch(
+                                 commentAction({
+                                    recommend: comment.recommend,
+                                    createdAt: comment.createdAt,
+                                    titleReview: comment.title,
+                                    verified: comment.verified,
+                                    dislike: comment.dislike,
+                                    imageUrl: comment.images,
+                                    review: comment.comment,
+                                    stars: comment.rating,
+                                    email: comment.email,
+                                    commentImages: true,
+                                    name: comment.name,
+                                    indexImage: index,
+                                    like: comment.like,
+                                    id: comment.id,
+                                 })
+                              )
+                           }}
+                        >
                            <Image
                               width={100}
                               height={100}
                               alt="comment image"
-                              className="h-[75px] w-[75px] cursor-pointer object-cover"
+                              className="h-[100px] w-[100px] cursor-pointer object-cover"
                               src={image}
                            />
                         </div>
