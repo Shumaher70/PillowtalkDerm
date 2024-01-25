947f7d0e9103e55d386ed899be08dec86619267f
oleg <olegvraby@gmail.com>
2024-01-25 19:41:58 +0200

    add:api/user: logic of verified for the user review

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -3,6 +3,36 @@ import { NextResponse } from "next/server"
 
 export const GET = async (request: Request) => {
    const user = await prisma.user.findMany()
+   const reviews = await prisma.review.findMany()
+
+   const userFilter = user.filter(
+      (user) => user.img?.length !== 0 && user.img?.length !== undefined
+   )
+
+   userFilter.forEach(async (user) => {
+      await prisma.user.updateMany({
+         where: {
+            id: user.id,
+         },
+         data: {
+            verified: true,
+         },
+      })
+   })
+
+   userFilter.forEach((users) => {
+      users.email
+      reviews.forEach(async (review) => {
+         if (review.email === users.email) {
+            await prisma.review.updateMany({
+               where: {
+                  email: review.email,
+               },
+               data: { verified: true },
+            })
+         }
+      })
+   })
 
    return NextResponse.json(user)
 }
