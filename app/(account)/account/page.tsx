94ffe8417fc80da7738b38a159447265b05b7af2
oleg <olegvraby@gmail.com>
2024-01-26 13:44:15 +0200

    change:account: remove useEffect

--- app/(account)/account/page.tsx
+++ app/(account)/account/page.tsx
@@ -2,16 +2,15 @@
 
 import { useUser } from "@clerk/nextjs"
 import { useRouter } from "next/navigation"
-import { useEffect } from "react"
 
 const Page = () => {
    const { isSignedIn } = useUser()
    const route = useRouter()
-   useEffect(() => {
-      if (!isSignedIn) {
-         route.push("/")
-      }
-   }, [isSignedIn, route])
+
+   if (!isSignedIn) {
+      route.push("/")
+   }
+
    return (
       <div className="flex-center flex h-full w-full text-[100px]">Account</div>
    )
