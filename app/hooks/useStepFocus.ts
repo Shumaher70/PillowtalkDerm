6044f7c723c57007fa1971ac3bfc4c35e1d9c0c4
oleg <olegvraby@gmail.com>
2024-01-26 13:43:02 +0200

    create:useStepFocus

---
+++ app/hooks/useStepFocus.ts
@@ -0,0 +1,19 @@
+import { RefObject, useEffect } from "react"
+
+const useStepFocus = (
+   stepSlice: number,
+   step: number,
+   refInput: RefObject<HTMLInputElement> | RefObject<HTMLTextAreaElement>,
+   delay: number
+) => {
+   useEffect(() => {
+      const timeout = setTimeout(() => {
+         if (stepSlice === step) {
+            refInput.current!.focus()
+         }
+      }, delay)
+      return () => clearTimeout(timeout)
+   }, [stepSlice])
+}
+
+export default useStepFocus
