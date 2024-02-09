16bd9c8dcba1310ebae6a211a3430ff482dc891a
oleg <olegvraby@gmail.com>
2024-02-09 18:11:16 +0200

    add:blogs/news/Ingredient-carousel/IngredientCarousel:added 1:created function "opacity style" for each element in carousel  which hidden 2:added route

--- app/(pages)/blogs/news/ingredients-glossary/components/IngredientsCarousel.tsx
+++ app/(pages)/blogs/news/ingredients-glossary/components/IngredientsCarousel.tsx
@@ -1,15 +1,51 @@
 "use client"
 import { schnyderMlightFont } from "@/app/layout"
+import Image from "next/image"
 import Carousel from "react-multi-carousel"
+import { IngredientsCarouselDummy } from "./IngredientsCarouselDummy"
+import { useCallback, useEffect, useState } from "react"
+import { useRouter } from "next/navigation"
 
 const IngredientsCarousel = () => {
+   const route = useRouter()
+   const [slide, setSlide] = useState({
+      showSlide: 0,
+      currentSlide: 0,
+   })
    const responsive = {
       desktop: {
-         breakpoint: { max: 3000, min: 0 },
+         breakpoint: { max: 3000, min: 1024 },
+         items: 3,
+         slidesToSlide: 1,
+      },
+      laptop: {
+         breakpoint: { max: 1024, min: 768 },
+         items: 2,
+         slidesToSlide: 2,
+      },
+      mobile: {
+         breakpoint: { max: 768, min: 0 },
          items: 1,
          slidesToSlide: 1,
       },
    }
+
+   const handleResize = useCallback(() => {
+      if (window.innerWidth <= 768) {
+         setSlide({ showSlide: 1, currentSlide: 0 })
+      } else if (window.innerWidth >= 768 && window.innerWidth <= 1024) {
+         setSlide({ showSlide: 2, currentSlide: 0 })
+      } else {
+         setSlide({ showSlide: 3, currentSlide: 0 })
+      }
+   }, [])
+
+   useEffect(() => {
+      handleResize()
+      window.addEventListener("resize", handleResize)
+
+      return () => window.removeEventListener("resize", handleResize)
+   }, [handleResize])
 
    const CustomDot = ({
       onClick,
@@ -26,13 +62,45 @@ const IngredientsCarousel = () => {
                   : "inactive"
             } border-1 mr-2 h-[8px] w-[8px] rounded-full border-purple-300`}
             onClick={() => onClick()}
-         ></button>
+         />
       )
+   }
+
+   const handlerOpacityElementStyle = (index: number) => {
+      let opacity
+      if (slide.currentSlide === 0 && slide.showSlide === 3 && index === 3) {
+         opacity = "opacity-50"
+      } else if (
+         slide.currentSlide === 1 &&
+         slide.showSlide === 3 &&
+         index === 0
+      ) {
+         opacity = "opacity-50"
+      } else if (
+         slide.currentSlide === 0 &&
+         slide.showSlide === 2 &&
+         index >= 2
+      ) {
+         opacity = "opacity-50"
+      } else if (
+         slide.currentSlide === 2 &&
+         slide.showSlide === 2 &&
+         index <= 1
+      ) {
+         opacity = "opacity-50"
+      } else if (slide.currentSlide !== index && slide.showSlide === 1) {
+         opacity = "opacity-50"
+      }
+
+      return opacity
    }
 
    return (
       <>
-         <div className="bg-secondary container-rounded flex flex-col items-center justify-center px-3 py-2 text-center md:px-5 lg:flex-row lg:items-center lg:justify-between">
+         <div
+            draggable="false"
+            className="bg-secondary container-rounded flex flex-col items-center justify-center px-3 py-2 text-center md:px-5 lg:flex-row lg:items-center lg:justify-between"
+         >
             <h1 className={`${schnyderMlightFont.className} text-section`}>
                Ingredients Glossary
             </h1>
@@ -40,25 +108,66 @@ const IngredientsCarousel = () => {
                learn more
             </button>
          </div>
-         <div className=" mt-5">
+         <div className="relative mt-5">
             <Carousel
+               beforeChange={(slide, ref) =>
+                  setSlide({
+                     showSlide: ref.slidesToShow,
+                     currentSlide: slide,
+                  })
+               }
+               rewindWithAnimation={false}
+               rtl={false}
+               rewind={false}
+               partialVisible
+               focusOnSelect={false}
+               containerClass="container-carousel overflow-visible  select-none"
+               itemClass="p-2"
+               additionalTransfrom={0}
+               centerMode={false}
                swipeable={true}
+               minimumTouchDrag={1}
                arrows={false}
                draggable={true}
                showDots={true}
+               renderDotsOutside
                responsive={responsive}
                ssr={true}
                infinite={false}
                keyBoardControl={true}
                transitionDuration={10}
-               containerClass="carousel-container"
-               dotListClass="block w-max h-[15px] absolute top-0 !left-[calc(100%-16px)] -translate-x-full"
-               itemClass="p-2"
-               className="!static select-none overflow-visible"
+               dotListClass="w-full "
                customDot={<CustomDot onClick={() => {}} active={false} />}
             >
-               <div className="bg-black">hello</div>
-               <div className="bg-red-500">hello</div>
+               {IngredientsCarouselDummy.map((i, index) => (
+                  <div
+                     onClick={() =>
+                        route.push("/blogs/news/ingredients-glossary")
+                     }
+                     key={index}
+                     className={`flex h-full cursor-pointer flex-col ${handlerOpacityElementStyle(
+                        index
+                     )} transition-all duration-500`}
+                  >
+                     <div className="container-rounded-t relative overflow-hidden pt-[80%] ">
+                        <Image
+                           draggable="false"
+                           width={0}
+                           height={0}
+                           sizes="100vh"
+                           className="absolute right-0 top-0 h-full w-full object-cover brightness-[0.8]"
+                           src={i.image}
+                           alt={`slider-${index + 1}`}
+                        />
+                        <h3 className="absolute bottom-7 left-3 text-[20px] font-medium leading-[20px] text-white md:bottom-8 md:text-[32px] md:leading-[32px] lg:bottom-10 lg:left-5 lg:text-[35px] lg:leading-[35px]">
+                           {i.title}
+                        </h3>
+                     </div>
+                     <div className="bg-secondary container-rounded text-p relative bottom-5 h-full overflow-hidden p-2 md:p-3 lg:p-5 lg:text-[20px] ">
+                        <p>{i.description}</p>
+                     </div>
+                  </div>
+               ))}
             </Carousel>
          </div>
       </>
