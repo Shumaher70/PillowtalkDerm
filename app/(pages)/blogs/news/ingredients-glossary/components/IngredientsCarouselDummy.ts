f87b444d38187c56260bca7ee2a5566189b3f213
oleg <olegvraby@gmail.com>
2024-02-09 17:59:09 +0200

    create:blogs/news/IngredientsCarouselDummy

---
+++ app/(pages)/blogs/news/ingredients-glossary/components/IngredientsCarouselDummy.ts
@@ -0,0 +1,26 @@
+export const IngredientsCarouselDummy = [
+   {
+      image: "/imagesIngredientsCarousel/1.webp",
+      title: "Arnica (Arnica Montana Flower Extract)",
+      description:
+         "This plant extract is traditionally used to help decrease the appearance of swelling and inflammation, as well as the look of bruising. It’s commonly used to expedite healing following in-office procedures, such as injections and lasers.",
+   },
+   {
+      image: "/imagesIngredientsCarousel/2.webp",
+      title: "Alpha-Arbutin",
+      description:
+         "A safe and effective ingredient for improving discoloration. Alpha arbutin is a synthesized version of arbutin, a derivative of hydroquinone that’s found in the leaves of plants such as bearberry and cranberry. It works by reducing the activity of tyrosinase (a naturally occurring enzyme that regulates melanin production in...",
+   },
+   {
+      image: "/imagesIngredientsCarousel/3.webp",
+      title: "4-Butyl Resorcinol",
+      description:
+         "This is a derivative of resorcinol, an ingredient that can be synthesized in a lab or created using extracts of plant resins (PillowtalkDerm products use a lab-created version). 4-Butylresorcinol improves discoloration by inhibiting the activity of tyrosinase (a naturally occurring enzyme that regulates melanin production in the skin), thereby...",
+   },
+   {
+      image: "/imagesIngredientsCarousel/4.webp",
+      title: "Agastache Mexicana Flower",
+      description:
+         "This species of flowering plant, also known as Hyssop, is an edible flower with an aroma. This ingredient has antioxidant and anti-inflammatory properties that help to calm visible skin redness.",
+   },
+]
