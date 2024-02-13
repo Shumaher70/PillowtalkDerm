4fdf56edf89b0ea91ac495fef3f865b12e6b497f
oleg <olegvraby@gmail.com>
2024-02-13 18:50:21 +0200

    create:IngredientData

---
+++ app/(pages)/blogs/news/ingredients-glossary/components/ingredientData.ts
@@ -0,0 +1,54 @@
+export const ingredientData = [
+   {
+      title: "4-Butyl Resorcinol",
+      details:
+         "This is a derivative of resorcinol, an ingredient that can be synthesized in a lab or created using extracts of plant resins (Dr. Idriss products use a lab-created version). 4-Butylresorcinol improves discoloration by inhibiting the activity of tyrosinase (a naturally occurring enzyme that regulates melanin production in the skin), thereby reducing the production of melanin.",
+      studies: [
+         "pubmed.ncbi.nlm.nih.gov/17388924/",
+         "ncbi.nlm.nih.gov/pmc/articles/PMC4725640/",
+         "ncbi.nlm.nih.gov/pmc/articles/PMC2883392/",
+         "pubmed.ncbi.nlm.nih.gov/23205541/",
+         "onlinelibrary.wiley.com/doi/full/10.1111/jdv.12051",
+      ],
+   },
+   {
+      title: "Ash Bark Extract (Fraxinus Excelsior Bark Extract)",
+      details:
+         "This bark of a species of ash tree is used as an antioxidant and anti-inflammatory ingredient. It's known to help reduce the appearance of puffiness.",
+      studies: [
+         "Cytobiol™ Lumin-Eye",
+         "ncbi.nlm.nih.gov/pmc/articles/PMC5723943/",
+      ],
+   },
+   {
+      title: "Avena Sativa Kernel Extract",
+      details:
+         "Also known as oat extract, this ingredient has antioxidant and anti-inflammatory properties. It’s known as a skin soother, especially for dry, itchy, and irritated skin. It removes the dirt and oils from the skin while helping to brighten and improve skin tone.",
+   },
+   {
+      title: "Centella Asiatica Extract",
+      details:
+         "This tropical medicinal plant extract with antioxidant properties is also known as cica, gotu kola, or tiger grass extract. This ingredient has a history of use for healing wounds but can also help with redness relief and hydration.",
+      studies: [
+         "ncbi.nlm.nih.gov/pmc/articles/PMC3116297/pdf/IJPhS-72-",
+         "546.pdfciencedirect.com/topics/agricultural-and-biological-sciences/centella-asiatica",
+      ],
+   },
+   {
+      title: "Diglucosyl Gallic Acid",
+      details:
+         "This lab-created molecule has a multi-pronged effect on discoloration. The proprietary version used in our products is called Brightenyl®, or Trihydroxybenzoic acid glucoside (THBG). When applied topically, it acts as an antioxidant, inhibiting the production of reactive oxygen species, or ROS (a type of free radical that triggers skin damage). It also has an anti-inflammatory effect, which can help to decrease redness, and it prevents UV-induced DNA damage that can lead to dark spots. Most importantly, it disrupts your skin’s pigmentation process by acting on the MITF (Melanocyte Inducing Transcription Factor) gene to control melanin production, inhibit melanin transfer to the top layer of skin, and prevent UV light from triggering more melanin production.",
+      studies: ["pubmed.ncbi.nlm.nih.gov/35211990/"],
+   },
+   {
+      title: "Glycerin",
+      details:
+         "This hydrating ingredient can be derived naturally from the fats in certain plants or created in a lab (we use a lab-created version). It’s a humectant, meaning it attracts moisture from the air and locks it into the top layer of the skin, leaving it more hydrated. It’s also a component of skin’s Natural Moisturizing Factors (NMFs).",
+      studies: [
+         "ncbi.nlm.nih.gov/pmc/articles/PMC165880/",
+         "ncbi.nlm.nih.gov/pmc/articles/PMC556056/",
+         "ncbi.nlm.nih.gov/pmc/articles/PMC5608132/",
+         "ncbi.nlm.nih.gov/pmc/articles/PMC3396453/",
+      ],
+   },
+]
