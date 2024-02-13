4b059121985f560e87f4b63fb42a90528c29c741
oleg <olegvraby@gmail.com>
2024-02-13 18:50:00 +0200

    add:blogs/news/ingredients-glossary/page: added content

--- app/(pages)/blogs/news/ingredients-glossary/page.tsx
+++ app/(pages)/blogs/news/ingredients-glossary/page.tsx
@@ -1,5 +1,33 @@
+"use client"
+
+import { schnyderMlightFont } from "@/app/layout"
+import IngredientGlossaryFilter from "./components/IngredientGlossaryFilter"
+
 const page = () => {
-   return <div>page</div>
+   return (
+      <main className="container-px bg-secondary">
+         <section className="section-pt">
+            <div className="w-full">
+               <h1
+                  className={`text-section leading-[45px] ${schnyderMlightFont.className}`}
+               >
+                  Ingredients Glossary
+               </h1>
+               <h5 className="mt-3 max-w-[600px] text-[16px] font-medium md:mt-5 lg:text-[18px]">
+                  We list all of the ingredients used in Dr. Idriss products by
+                  their common names, which is how Dr. Shereene Idriss usually
+                  refers to them in conversation. We also list the INCI
+                  (International Nomenclature of Cosmetic Ingredients) name in
+                  parenthesis if it differs from the common name.
+               </h5>
+            </div>
+         </section>
+
+         <section className="mt-5">
+            <IngredientGlossaryFilter />
+         </section>
+      </main>
+   )
 }
 
 export default page
