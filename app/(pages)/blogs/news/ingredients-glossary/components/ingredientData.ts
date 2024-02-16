e3330bed53905d309aec36f70613acdb37e3ed35
oleg <olegvraby@gmail.com>
2024-02-16 15:40:49 +0200

    add:ingredientData: add more data

--- app/(pages)/blogs/news/ingredients-glossary/components/ingredientData.ts
+++ app/(pages)/blogs/news/ingredients-glossary/components/ingredientData.ts
@@ -12,12 +12,39 @@ export const ingredientData = [
       ],
    },
    {
+      title: "Agastache Mexicana Flower",
+      details:
+         "This species of flowering plant, also known as Hyssop, is an edible flower with an aroma. This ingredient has antioxidant and anti-inflammatory properties that help to calm visible skin redness.",
+      studies: [
+         "freshlycosmetics.com/en/glossary-of-ingredients/mexican-agastache",
+         "ncbi.nlm.nih.gov/pmc/articles/PMC8234942/",
+      ],
+   },
+   {
+      title: "Alpha-Arbutin",
+      details:
+         "A safe and effective ingredient for improving discoloration. Alpha arbutin is a synthesized version of arbutin, a derivative of hydroquinone that’s found in the leaves of plants such as bearberry and cranberry. It works by reducing the activity of tyrosinase (a naturally occurring enzyme that regulates melanin production in the skin) to inhibit melanin production. Alpha arbutin has also been shown to have antioxidant activity that enhances its skin-brightening properties.",
+      studies: [
+         "https://onlinelibrary.wiley.com/doi/abs/10.1111/j.1346-8138.2008.00522.x",
+      ],
+   },
+   {
+      title: "Arnica (Arnica Montana Flower Extract)",
+      details:
+         "This plant extract is traditionally used to help decrease the appearance of swelling and inflammation, as well as the look of bruising. It’s commonly used to expedite healing following in-office procedures, such as injections and lasers.",
+      studies: [
+         "https://ncbi.nlm.nih.gov/pmc/articles/PMC9251990/",
+         "https://pubmed.ncbi.nlm.nih.gov/20412090/",
+         "https://pubmed.ncbi.nlm.nih.gov/25171757/",
+      ],
+   },
+   {
       title: "Ash Bark Extract (Fraxinus Excelsior Bark Extract)",
       details:
          "This bark of a species of ash tree is used as an antioxidant and anti-inflammatory ingredient. It's known to help reduce the appearance of puffiness.",
       studies: [
-         "Cytobiol™ Lumin-Eye",
-         "ncbi.nlm.nih.gov/pmc/articles/PMC5723943/",
+         "https://Cytobiol™ Lumin-Eye",
+         "https://ncbi.nlm.nih.gov/pmc/articles/PMC5723943/",
       ],
    },
    {
@@ -30,25 +57,98 @@ export const ingredientData = [
       details:
          "This tropical medicinal plant extract with antioxidant properties is also known as cica, gotu kola, or tiger grass extract. This ingredient has a history of use for healing wounds but can also help with redness relief and hydration.",
       studies: [
-         "ncbi.nlm.nih.gov/pmc/articles/PMC3116297/pdf/IJPhS-72-",
-         "546.pdfciencedirect.com/topics/agricultural-and-biological-sciences/centella-asiatica",
+         "https://ncbi.nlm.nih.gov/pmc/articles/PMC3116297/pdf/IJPhS-72-",
+         "https://ciencedirect.com/topics/agricultural-and-biological-sciences/centella-asiatica",
       ],
    },
    {
       title: "Diglucosyl Gallic Acid",
       details:
          "This lab-created molecule has a multi-pronged effect on discoloration. The proprietary version used in our products is called Brightenyl®, or Trihydroxybenzoic acid glucoside (THBG). When applied topically, it acts as an antioxidant, inhibiting the production of reactive oxygen species, or ROS (a type of free radical that triggers skin damage). It also has an anti-inflammatory effect, which can help to decrease redness, and it prevents UV-induced DNA damage that can lead to dark spots. Most importantly, it disrupts your skin’s pigmentation process by acting on the MITF (Melanocyte Inducing Transcription Factor) gene to control melanin production, inhibit melanin transfer to the top layer of skin, and prevent UV light from triggering more melanin production.",
-      studies: ["pubmed.ncbi.nlm.nih.gov/35211990/"],
+      studies: ["https://pubmed.ncbi.nlm.nih.gov/35211990/"],
    },
    {
       title: "Glycerin",
       details:
          "This hydrating ingredient can be derived naturally from the fats in certain plants or created in a lab (we use a lab-created version). It’s a humectant, meaning it attracts moisture from the air and locks it into the top layer of the skin, leaving it more hydrated. It’s also a component of skin’s Natural Moisturizing Factors (NMFs).",
       studies: [
-         "ncbi.nlm.nih.gov/pmc/articles/PMC165880/",
-         "ncbi.nlm.nih.gov/pmc/articles/PMC556056/",
-         "ncbi.nlm.nih.gov/pmc/articles/PMC5608132/",
-         "ncbi.nlm.nih.gov/pmc/articles/PMC3396453/",
+         "https://ncbi.nlm.nih.gov/pmc/articles/PMC165880/",
+         "https://ncbi.nlm.nih.gov/pmc/articles/PMC556056/",
+         "https://ncbi.nlm.nih.gov/pmc/articles/PMC5608132/",
+         "https://ncbi.nlm.nih.gov/pmc/articles/PMC3396453/",
       ],
+   },
+   {
+      title: "Glycolic Acid",
+      details:
+         "This is an alpha-hydroxy acid (AHA) that is primarily used to exfoliate skin and stimulate collagen production. It occurs naturally in plants, such as sugarcane, and can also be produced in a lab (our products use a lab-created version). As an AHA, it works by breaking down the adhesions between cells in the upper layer of skin, allowing old skin cells to slough off. As a collagen stimulator, glycolic acid works by stimulating fibroblasts, cells that produce collagen.",
+      studies: [
+         "https://pubmed.ncbi.nlm.nih.gov/31944519/",
+         "https://pubmed.ncbi.nlm.nih.gov/8651713/",
+         "https://pubmed.ncbi.nlm.nih.gov/14756523/",
+         "https://pubmed.ncbi.nlm.nih.gov/33458927/",
+         "https://pubmed.ncbi.nlm.nih.gov/19695482/",
+      ],
+   },
+   {
+      title: "Hexapeptide-2",
+      details:
+         "This is a lab-created anti-aging peptide. The proprietary version used in our products is called DermostatylIS™. When applied topically, it binds to α-MSH (Alpha Melanocyte-Stimulating Hormone), which is a naturally occurring neuropeptide in your body that stimulates skin pigmentation. By doing so, it reduces melanin synthesis and decreases the melanin in skin cells that causes dark spots. In clinical studies, it’s been shown to brighten skin tone and reduce skin redness and also improve the appearance of fine lines and wrinkles.",
+      studies: [
+         "https://www.ashland.com/industries/personal-and-home-care/skin-and-sun-care/dermostatyl-is-biofunctional",
+      ],
+   },
+   {
+      title: "Kojic Acid",
+      details:
+         "A natural byproduct of fermentation, this ingredient is primarily used to help reduce discoloration. It works by inhibiting the activity of tyrosinase (a naturally occurring enzyme that regulates melanin production in the skin).",
+      studies: ["https://pubmed.ncbi.nlm.nih.gov/23716817/"],
+   },
+   {
+      title: "Lactic Acid",
+      details:
+         "Like glycolic acid, this is an alpha hydroxy acid (AHA) that exfoliates, stimulates collagen production, and also has moisturizing properties. It occurs naturally in sour milk products and can also be produced in a lab (our products use a lab-created version). As an exfoliator, it works by breaking down the adhesions between cells in the upper layer of skin, allowing old skin cells to slough off. Used in slightly higher doses (as in Major Fade Flash Mask), it can inhibit the activity of tyrosinase (a naturally occurring enzyme that regulates melanin production in the skin), thereby reducing abnormal pigmentation so skin looks brighter. It’s also a component of skin’s Natural Moisturizing Factors (NMFs) and can function as a humectant, leaving skin more hydrated.",
+      studies: [
+         "https://pubmed.ncbi.nlm.nih.gov/31944519",
+         "https://pubmed.ncbi.nlm.nih.gov/8651713/",
+         "https://pubmed.ncbi.nlm.nih.gov/14756523/",
+         "https://pubmed.ncbi.nlm.nih.gov/15762205/",
+      ],
+   },
+   {
+      title: "Niacinamide",
+      details:
+         "This form of vitamin B3 can reduce inflammation, fortify the skin barrier, improve skin texture, and improve discoloration. It works by strengthening the top layer of skin, improving the quality and thickness of skin, and protecting against transepidermal water loss (TEWL). It has also been shown to fade existing discoloration and can also reduce future discoloration by inhibiting the movement of melanin to the surface of the skin.",
+      studies: [
+         "https://pubmed.ncbi.nlm.nih.gov/16029679/",
+         "https://pubmed.ncbi.nlm.nih.gov/12100180/",
+         "https://pubmed.ncbi.nlm.nih.gov/15946237/",
+         "https://pubmed.ncbi.nlm.nih.gov/18492135/",
+      ],
+   },
+   {
+      title: "Rapeseed Extract (Brassica Napus Seedcake)",
+      details:
+         "This skin-brightening botanical extract is from the oil-rich seeds of rapeseed, a plant in the cabbage family. The proprietary version used in our products is called Achromaxyl ISR™, and it was designed to help optimize the activity of tyrosinase (a naturally occurring enzyme that regulates melanin production in the skin) in order to limit skin darkening in the presence of UV light and decrease the color intensity of existing age spots. In clinical tests, it has been shown to increase skin luminosity, brighten skin tone, and fade age spots.",
+      studies: [
+         "https://koreascience.kr/article/JAKO201322658549905.page",
+         "https://ashland.com/industries/personal-and-home-care/skin-and-sun-care/achromaxyl-isr-biofunctional",
+      ],
+   },
+   {
+      title: "Tranexamic Acid",
+      details:
+         "This is a derivative of the amino acid lysine that has been shown to decrease skin discoloration when applied topically. It is thought to improve skin discoloration by reducing the effects of MITF (Melanocyte Inducing Transcription Factor), in part by inhibiting the body’s plasminogen/plasmin system, which plays an important role in numerous processes, including wound healing and the modulation of blood flow.",
+      studies: [
+         "https://pubmed.ncbi.nlm.nih.gov/31531176/",
+         "https://pubmed.ncbi.nlm.nih.gov/28374042/",
+         "https://pubmed.ncbi.nlm.nih.gov/28283893/",
+      ],
+   },
+   {
+      title: "Vitamin C (TetraHexyldecyl Ascorbate or THD)",
+      details:
+         "Vitamin C comes in many forms and has multiple skin benefits. TetraHexyldecyl Ascorbate, a lab-created form of the vitamin, is oil-soluble, meaning it can penetrate the skin more effectively than water soluble versions. When formulated at the correct pH, as in our products, it is also highly stable. This form of vitamin C also acts as an antioxidant, essential cofactor in collagen synthesis, and exfoliator, promoting cell turnover. Plus, it can brighten skin tone by decreasing the activity of tyrosinase (a naturally occurring enzyme that regulates melanin production in the skin), which helps to inhibit irregular production of melanin.",
+      studies: ["https://pubmed.ncbi.nlm.nih.gov/33058522/"],
    },
 ]
