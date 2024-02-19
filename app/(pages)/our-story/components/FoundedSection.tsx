972086016589097cf34f7ad2bebf304db24e7055
oleg <olegvraby@gmail.com>
2024-02-19 13:05:26 +0200

    add:our-story/FoundedSection:added content

--- app/(pages)/our-story/components/FoundedSection.tsx
+++ app/(pages)/our-story/components/FoundedSection.tsx
@@ -1,5 +1,15 @@
 const FoundedSection = () => {
-   return <div>FoundedSection</div>
+   return (
+      <div className="container-p">
+         <div className="bg-accent container-rounded container-px py-[15px] text-center md:py-[24px]">
+            <h1 className="text-section leading-[40px] md:leading-[56px]">
+               Skin is complex.
+            </h1>
+            <h1 className="text-section mt-2 leading-[40px] md:mt-0 md:leading-[56px]">{`Skincare doesn't have to be.`}</h1>
+            <h5 className="pt-5 text-[16px] font-medium md:text-[18px]">{`At Dr. Idriss, we're over the hype and fearmongering. We put formulation first and use all of our performance ingredients at the most effective levels for your skin. Dr. Shereene Idriss' commitment to science means that all Dr. Idriss products are clinically tested, dermatologist tested, allergy tested, non-irritating, and fragrance-free.`}</h5>
+         </div>
+      </div>
+   )
 }
 
 export default FoundedSection
