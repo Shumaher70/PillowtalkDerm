e91c5343367024b263200141ea2847e9c3a09733
oleg <olegvraby@gmail.com>
2024-02-20 20:07:35 +0200

    create:pages/faq/page

---
+++ app/(pages)/pages/faq/page.tsx
@@ -0,0 +1,13 @@
+import FaqScroll from "./components/FaqScroll"
+
+const page = () => {
+   return (
+      <main className="bg-secondary w-full">
+         <div className="mt-[15px] pt-[40px] md:pt-[80px] lg:mt-[30px]">
+            <FaqScroll />
+         </div>
+      </main>
+   )
+}
+
+export default page
