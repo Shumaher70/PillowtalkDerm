3a97c3e10c9c6ec2bb5de75cc9a386e54da1847b
oleg <olegvraby@gmail.com>
2024-02-22 18:41:15 +0200

    add:navbar/cart/CheckOut:added "axios.post" with cart data to button and route to strip checkout page

--- app/components/navbar/components/cart/components/checkOut/CheckOut.tsx
+++ app/components/navbar/components/cart/components/checkOut/CheckOut.tsx
@@ -1,21 +1,49 @@
-import Button from "@/app/components/button/Button"
+"use client"
+
+import { useAppSelector } from "@/redux/hooks"
+import axios from "axios"
+import { useRouter } from "next/navigation"
 
 interface CheckOutProps {
    totalPrice: number
 }
 
 const CheckOut = ({ totalPrice }: CheckOutProps) => {
+   const cardSlice = useAppSelector((state) => state.cartReducer.carts)
+
+   const route = useRouter()
+
    return (
       <div className="flex justify-between rounded-t-[8px] bg-white p-[16px] shadow-[0_-1px_10px_rgba(0,0,0,0.26)]">
          <div>
             <p className="text-p text-bold font-sans">Estimated Total</p>
             <p>Taxes and shipping calculated at checkout</p>
-            <Button
-               text="checkout"
-               size="sm"
-               className="mt-4 bg-purple-800 uppercase"
-               load={false}
-            />
+            <button
+               onClick={async () =>
+                  axios
+                     .post("http://localhost:3000/api/checkout", {
+                        products: cardSlice,
+                     })
+                     .then((response) => {
+                        if (response.data) {
+                           route.push(response.data.url)
+                        }
+                     })
+               }
+               className="flex-center !duration-250
+               relative
+               mt-4
+               cursor-pointer
+               rounded-full
+               bg-purple-800
+               px-[16px]
+               py-[10px]
+               uppercase
+               text-white transition-all hover:bg-pink-600
+               "
+            >
+               checkout
+            </button>
          </div>
 
          <p className="text-p text-bold font-sans">${totalPrice}</p>
