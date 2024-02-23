4612fabe8583665a68eabc4dd40623ba2aa91847
oleg <olegvraby@gmail.com>
2024-02-23 19:13:23 +0200

    change:account/page: remove all content

--- app/(account)/account/page.tsx
+++ app/(account)/account/page.tsx
@@ -1,16 +1,4 @@
-"use client"
-
-import { useUser } from "@clerk/nextjs"
-import { useRouter } from "next/navigation"
-
 const Page = () => {
-   const { isSignedIn } = useUser()
-   const route = useRouter()
-
-   if (!isSignedIn) {
-      route.push("/")
-   }
-
    return (
       <div className="flex-center flex h-full w-full text-[100px]">Account</div>
    )
