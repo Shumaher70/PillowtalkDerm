8a37a9dc7dc4fe57c330fc2643da363d81d7fc08
oleg <olegvraby@gmail.com>
2024-02-26 19:03:50 +0200

    add:account/page:get url query, added component Success and transmitted props

--- app/(account)/account/page.tsx
+++ app/(account)/account/page.tsx
@@ -1,6 +1,13 @@
-const Page = () => {
+import Success from "./Success"
+
+const Page = async (query: { searchParams: { userid: string } }) => {
+   const { searchParams } = query
+   const { userid } = searchParams
+
    return (
-      <div className="flex-center flex h-full w-full text-[100px]">Account</div>
+      <div className="flex-center flex h-full w-full text-[100px]">
+         <Success userId={userid} />
+      </div>
    )
 }
 
