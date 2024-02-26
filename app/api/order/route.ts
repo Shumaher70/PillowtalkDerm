efeabdaf1310ecd5cd171eb6d7b3e745ed2e3ec1
oleg <olegvraby@gmail.com>
2024-02-26 19:01:03 +0200

    add:api/order:added get

--- app/api/order/route.ts
+++ app/api/order/route.ts
@@ -1,8 +1,29 @@
-import Order from "@/app/(pages)/products/[title]/components/card/components/order/Order"
 import { prisma } from "@/lib/prisma"
 import { auth } from "@clerk/nextjs"
+import { Order } from "@prisma/client"
 
 import { NextResponse } from "next/server"
+
+export async function GET(request: Request) {
+   const { userId } = auth()
+
+   try {
+      const orders: Order[] = await prisma.order.findMany({
+         where: {
+            userId: userId as string,
+         },
+      })
+
+      return NextResponse.json(orders)
+   } catch (error: any) {
+      if (error.code === "P2002") {
+         return new NextResponse("can't get orders", {
+            status: 409,
+         })
+      }
+      return new NextResponse(error.message, { status: 500 })
+   }
+}
 
 export async function POST(request: Request) {
    const { userId } = auth()
@@ -23,7 +44,7 @@ export async function POST(request: Request) {
             })
       )
 
-      return new NextResponse(JSON.stringify(true), {
+      return new NextResponse(JSON.stringify(users), {
          status: 201,
          headers: { "Content-Type": "application/json" },
       })
