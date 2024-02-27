82968c184d0f0da18e167058e89e67cc473fc759
oleg <olegvraby@gmail.com>
2024-02-27 16:32:45 +0200

    add:account:added title and button back to home

--- app/(account)/account/page.tsx
+++ app/(account)/account/page.tsx
@@ -1,13 +1,26 @@
+import { IoIosHome } from "react-icons/io"
+
+import Link from "next/link"
 import Success from "./Success"
+import { Suspense } from "react"
 
 const Page = async (query: { searchParams: { userid: string } }) => {
    const { searchParams } = query
    const { userid } = searchParams
 
    return (
-      <div className="flex-center flex h-full w-full text-[100px]">
+      <main className="container-p h-full !pt-[30px]">
+         <div className="mb-5 flex flex-col items-center justify-between gap-2 md:flex-row">
+            <h1 className="text-section ">My orders</h1>
+            <Link
+               className="flex-center flex cursor-pointer gap-1 rounded-full bg-pink-600 px-4 py-2 text-[16px] uppercase text-white"
+               href="/"
+            >
+               to home {<IoIosHome className="text-white" />}
+            </Link>
+         </div>
          <Success userId={userid} />
-      </div>
+      </main>
    )
 }
 
