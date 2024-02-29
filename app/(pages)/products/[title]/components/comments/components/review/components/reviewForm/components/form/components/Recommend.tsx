214df98646121b6cdfc9af2527efcc6a04d4b447
oleg <olegvraby@gmail.com>
2024-02-29 13:04:29 +0200

    change:products/comments/review/form/Recommend: added react query

--- app/(pages)/products/[title]/components/comments/components/review/components/reviewForm/components/form/components/Recommend.tsx
+++ app/(pages)/products/[title]/components/comments/components/review/components/reviewForm/components/form/components/Recommend.tsx
@@ -8,43 +8,33 @@ import {
    refreshAction,
    stepAction,
 } from "@/redux/features/commentSlice"
-import { useCallback, useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 import { FaCheck } from "react-icons/fa6"
 import { reviewForm } from "@/redux/features/sidebarSlice"
 import axios from "axios"
+import { useMutation } from "@tanstack/react-query"
 
 const Recommend = () => {
-   const [text, setText] = useState("submit")
+   const [test, setTest] = useState("submit")
    const [checked, setChecked] = useState(true)
-   const [checkedEnter, setCheckedEnter] = useState(false)
-   const [isSubmitting, setIsSubmitting] = useState(false)
-
    const stepSlice = useAppSelector((state) => state.commentSlice.review.step!)
    const commentSlice = useAppSelector((state) => state.commentSlice.review)
    const dispatch = useAppDispatch()
 
-   const leaveHandler = () => {
-      setCheckedEnter(false)
-   }
-   const handlerCheck = () => {
-      setChecked((previous) => !previous)
-      setCheckedEnter(true)
-   }
-
-   const handlerTextSubmit = useCallback(() => {
-      setIsSubmitting(true)
-      setText("loading...")
-
-      const postTimeout = setTimeout(async () => {
-         setText("success!")
-         await axios.post("/api/user", {
+   const handleUserPost = async () => {
+      return await axios
+         .post("/api/user", {
             first_name: commentSlice.name,
             last_name: commentSlice.name,
             email: commentSlice.email,
             img: "",
          })
+         .then((response) => response.data)
+   }
 
-         await axios.post("/api/reviews", {
+   const handleReviewPost = async () => {
+      return await axios
+         .post("/api/reviews", {
             rating: commentSlice.stars,
             title: commentSlice.titleReview,
             comment: commentSlice.review,
@@ -55,57 +45,73 @@ const Recommend = () => {
             recommend: commentSlice.recommend,
             email: commentSlice.email,
          })
-      }, 1900)
-      const refreshActionTimeout = setTimeout(() => {
-         dispatch(reviewForm(false))
-         dispatch(refreshAction())
-         setText("submit")
-         setIsSubmitting(false)
-      }, 2000)
+         .then((response) => response.data)
+   }
 
-      return () => {
-         clearTimeout(postTimeout)
-         clearTimeout(refreshActionTimeout)
-      }
-   }, [
-      commentSlice.email,
-      commentSlice.imageKey,
-      commentSlice.imageUrl,
-      commentSlice.name,
-      commentSlice.productId,
-      commentSlice.recommend,
-      commentSlice.review,
-      commentSlice.stars,
-      commentSlice.titleReview,
-      dispatch,
-   ])
+   const {
+      mutate: mutateUser,
+      isPending: isPendingUser,
+      isSuccess: isSuccessUser,
+   } = useMutation({
+      mutationFn: handleUserPost,
+      mutationKey: [commentSlice],
+   })
 
-   const handleUserKeyPress = useCallback(
-      async (event: { key: any; keyCode: any }) => {
+   const {
+      mutate: mutateReview,
+      isPending: isPendingReview,
+      isSuccess: isSuccessReview,
+   } = useMutation({
+      mutationFn: handleReviewPost,
+      mutationKey: [commentSlice],
+   })
+
+   useEffect(() => {
+      const handleUserKeyPress = async (event: { key: any; keyCode: any }) => {
          const { key } = event
 
-         if (key === "Enter" && stepSlice === 7 && !isSubmitting) {
+         if (key === "Enter" && stepSlice === 7) {
             dispatch(recommendAction(checked))
-            handlerTextSubmit()
+            mutateUser()
+            mutateReview()
          }
          if (key === "Escape" && stepSlice === 7) {
             dispatch(stepAction(6))
          }
-      },
-      [checked, dispatch, handlerTextSubmit, isSubmitting, stepSlice]
-   )
+      }
 
-   useEffect(() => {
       window.addEventListener("keydown", handleUserKeyPress)
       return () => {
          window.removeEventListener("keydown", handleUserKeyPress)
       }
-   }, [handleUserKeyPress])
+   }, [checked, dispatch, mutateReview, mutateUser, stepSlice])
 
    useEffect(() => {
+      if (isPendingUser && isPendingReview) {
+         setTest("sending...")
+      } else if (isSuccessUser && isSuccessReview) {
+         setTest("success")
+      }
       dispatch(recommendAction(checked))
-   }, [checked, dispatch])
+   }, [
+      checked,
+      dispatch,
+      isPendingReview,
+      isPendingUser,
+      isSuccessReview,
+      isSuccessUser,
+   ])
 
+   const handlerCheck = () => {
+      setChecked((previous) => !previous)
+   }
+
+   if (isSuccessUser && isSuccessReview) {
+      setTimeout(() => {
+         dispatch(refreshAction())
+         dispatch(reviewForm(false))
+      }, 1000)
+   }
    return (
       <motion.div
          initial={{
@@ -121,13 +127,10 @@ const Recommend = () => {
          <div
             className="flex min-w-max cursor-pointer items-center justify-center gap-2"
             onClick={handlerCheck}
-            onMouseLeave={leaveHandler}
          >
             <div className="flex-center relative flex h-[20px] w-[20px] bg-white outline outline-1 outline-gray-100">
                <div
-                  className={`checkShadow shadow-2xl] absolute right-0 top-0 h-[20px] w-[20px] rounded-[2px] bg-white transition-all duration-[250ms] ${
-                     checkedEnter ? "opacity-1" : "opacity-0"
-                  }`}
+                  className={`checkShadow absolute right-0 top-0 z-10 h-[20px] w-[20px] rounded-[2px] opacity-0 shadow-2xl transition-all duration-[250ms] hover:opacity-[1]`}
                />
                <FaCheck
                   className={`bg-accent relative z-[2] h-full w-full !bg-[#F92672] p-[2px] text-white ${
@@ -138,14 +141,21 @@ const Recommend = () => {
             <p>i recommend this product</p>
          </div>
          <button
-            disabled={text !== "submit" ? true : false}
+            disabled={isPendingUser && isPendingReview}
             onClick={() => {
                dispatch(recommendAction(checked))
-               handlerTextSubmit()
+               mutateUser()
+               mutateReview()
             }}
-            className="mt-5 cursor-pointer rounded-full bg-purple-700 px-[16px] py-[8px] uppercase text-white"
+            className={`mt-5 cursor-pointer rounded-full px-[16px] py-[8px] uppercase text-white ${
+               isPendingUser && isPendingReview ? "opacity-50" : "opacity-1"
+            } ${
+               isSuccessUser && isSuccessReview
+                  ? "!bg-green-500"
+                  : "bg-purple-700"
+            }`}
          >
-            {text}
+            {test}
          </button>
 
          <div className="absolute -bottom-[20px] mt-10 w-full max-w-[470px] text-center text-[13px] text-gray-700">
