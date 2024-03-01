88f5b61b4ce37bcaf76d31e4f9e61745b269eab1
oleg <olegvraby@gmail.com>
2024-03-01 16:20:28 +0200

    change: Navbar: 1.added react query 2.made the trigger state and apply it for SlideInfo , Burger, SearchMobile 3.

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -26,48 +26,46 @@ import LoginMenu from "../LoginMenu"
 import Overflow from "../Overflow"
 import SlideInfo from "../slideInfo/SlideInfo"
 import CommentImage from "../commentImage/CommentImage"
+import axios from "axios"
+import { useQuery } from "@tanstack/react-query"
+
+const handleProductsGet = async () => {
+   const products: ProductType[] = await axios
+      .get("http://localhost:3000/api/products")
+      .then((response) => response.data)
+
+   return products
+}
+
+const handleBlogsGet = async () => {
+   const blogs: BlogType[] = await axios
+      .get("/api/blog")
+      .then((response) => response.data)
+   return blogs
+}
 
 const Navbar = () => {
    const dispatch = useDispatch()
-   const [products, setProducts] = useState<ProductType[]>([])
-   const [blogs, setBlogs] = useState<BlogType[]>([])
+   const [navbarTrigger, setNavbarTrigger] = useState<boolean>(false)
 
-   useEffect(() => {
-      let subscribe = true
-      const fetchData = async () => {
-         try {
-            const [productsRes, blogsRes] = await Promise.all([
-               fetch("/api/products"),
-               fetch("/api/blog"),
-            ])
+   const { data: blogs } = useQuery({
+      queryKey: ["getBlogs"],
+      queryFn: handleBlogsGet,
+   })
 
-            if (productsRes.ok) {
-               const productsData = await productsRes.json()
-               setProducts(productsData)
-            }
-
-            if (blogsRes.ok) {
-               const blogsData = await blogsRes.json()
-               setBlogs(blogsData)
-            }
-         } catch (error) {
-            console.error("Error fetching data:", error)
-         }
-      }
-
-      if (subscribe) {
-         fetchData()
-      }
-      return () => {
-         subscribe = false
-      }
-   }, [])
+   const { data: products } = useQuery({
+      queryKey: ["getProducts"],
+      queryFn: handleProductsGet,
+   })
 
    useEffect(() => {
       const widthHandler = () => {
          if (window.innerWidth >= 1024) {
             dispatch(sidebarBurger(false))
             dispatch(sidebarSearch(false))
+            setNavbarTrigger(true)
+         } else {
+            setNavbarTrigger(false)
          }
       }
       widthHandler()
@@ -75,12 +73,11 @@ const Navbar = () => {
 
       return () => window.removeEventListener("resize", widthHandler)
    }, [])
-
    return (
       <>
          <Overflow />
          <CommentImage />
-         <SlideInfo products={products} blogs={blogs} />
+         {navbarTrigger && <SlideInfo products={products} blogs={blogs} />}
          <LoginMenu />
          {
             <motion.header
@@ -88,28 +85,23 @@ const Navbar = () => {
                   dispatch(slideShop(false))
                   dispatch(slideSkinNerdAcademy(false))
                }}
-               className={`
-               container-px
-               container-rounded-b
-               bg-accent
-               fixed
-               top-0 
-               z-10
-               w-full
-               overflow-hidden 
-               py-[8px]
-               md:!px-[40px]
-               `}
+               className="container-px container-rounded-b bg-accent fixed top-0 z-10 w-full overflow-hidden py-[8px] md:!px-[40px]"
             >
                <nav className="flex-between flex w-full md:h-[72px]">
                   <div className="flex w-full justify-start gap-3">
                      <Info className="hidden lg:flex" />
-                     <Burger
-                        className="lg:hidden"
-                        products={products}
-                        blogs={blogs}
-                     />
-                     <SearchMobile className="block lg:hidden" />
+                     {!navbarTrigger && (
+                        <>
+                           <Burger
+                              className="lg:hidden"
+                              products={products}
+                              blogs={blogs}
+                           />
+                           <div className="lg:hidden">
+                              <SearchMobile />
+                           </div>
+                        </>
+                     )}
                   </div>
 
                   <div className="flex-center flex w-full">
@@ -117,17 +109,17 @@ const Navbar = () => {
                   </div>
 
                   <div className="flex w-full items-center justify-end gap-3">
-                     <div className="hidden lg:block">
-                        <CiSearch
-                           className={`cursor-pointer text-[30px] `}
-                           onClick={() => {
-                              dispatch(slideSearch(true))
-                              dispatch(slideShop(false))
-                              dispatch(slideSkinNerdAcademy(false))
-                           }}
-                        />
-                     </div>
+                     <CiSearch
+                        className="hidden cursor-pointer text-[30px] lg:block"
+                        onClick={() => {
+                           dispatch(slideSearch(true))
+                           dispatch(slideShop(false))
+                           dispatch(slideSkinNerdAcademy(false))
+                        }}
+                     />
+
                      <Login />
+
                      <Cart products={products} />
                   </div>
                </nav>
