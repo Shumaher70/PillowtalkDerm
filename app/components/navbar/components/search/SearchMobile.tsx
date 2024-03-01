00ab970280c550ecf283f331035422d15015d35e
oleg <olegvraby@gmail.com>
2024-03-01 14:04:20 +0200

    change:searchMobile: 1.remove load state 2. added reactQuery 3.added cleanInput function and put it to props in the Input component 4.added a check "if data has empty array" 5.added loading spin

--- app/components/navbar/components/search/SearchMobile.tsx
+++ app/components/navbar/components/search/SearchMobile.tsx
@@ -1,6 +1,6 @@
 "use client"
-import { useCallback, useEffect, useMemo, useState, useTransition } from "react"
-import { AiOutlineClose } from "react-icons/ai"
+import { useCallback, useEffect, useState } from "react"
+import { AiOutlineClose, AiOutlineLoading3Quarters } from "react-icons/ai"
 import { CiSearch } from "react-icons/ci"
 
 import Sidebar from "../../Sidebar"
@@ -8,10 +8,14 @@ import Input from "./components/Input"
 import TopSearches from "./components/TopSearches"
 import Card from "@/app/components/card/Card"
 import BlogCard from "@/app/components/blogCard/BlogCard"
+
 import { useAppDispatch, useAppSelector } from "@/redux/hooks"
 import { sidebarSearch } from "@/redux/features/sidebarSlice"
+
 import { BlogType, ProductType } from "@/types"
+
 import axios from "axios"
+import { useQuery } from "@tanstack/react-query"
 interface SearchProps {
    className?: string
 }
@@ -22,46 +26,34 @@ const Search = ({ className }: SearchProps) => {
 
    const [input, setInput] = useState("")
    const [postInput, setPostInput] = useState("")
-   const [load, setLoad] = useState(false)
-
-   const [data, setData] = useState<{
+   const [emptyFilterData, setEmptyFilterData] = useState<{
       products: ProductType[]
       blogs: BlogType[]
    }>()
-   const { products, blogs } = data || { products: [], blogs: [] }
+
+   const fetchData = async () => {
+      return await axios
+         .get(`http://localhost:3000/api/productsFilter?filter=${input}`)
+         .then(
+            (response) =>
+               response.data as { products: ProductType[]; blogs: BlogType[] }
+         )
+   }
+
+   const { data, isSuccess, isPending } = useQuery({
+      queryKey: [input],
+      queryFn: fetchData,
+   })
 
    useEffect(() => {
-      const loadHandler = () => {
-         if (window.innerWidth < 1024) {
-            setLoad(true)
-         } else {
-            setLoad(false)
-         }
+      if (
+         isSuccess &&
+         data &&
+         (data.products.length > 0 || data.blogs.length > 0)
+      ) {
+         setEmptyFilterData(data)
       }
-      loadHandler()
-      window.addEventListener("resize", loadHandler)
-      return () => window.removeEventListener("resize", loadHandler)
-   }, [])
-
-   useEffect(() => {
-      let subscribe = true
-      const fetchData = () => {
-         axios
-            .get(
-               `http://localhost:3000/api/productsFilter?filter=${
-                  input || postInput
-               }`
-            )
-            .then((data) => setData(data.data))
-      }
-
-      if (subscribe) {
-         fetchData()
-      }
-      return () => {
-         subscribe = false
-      }
-   }, [input, postInput])
+   }, [isSuccess, data])
 
    const getInput = useCallback((event: string) => {
       setInput(event)
@@ -71,82 +63,135 @@ const Search = ({ className }: SearchProps) => {
       setPostInput(event)
    }, [])
 
+   const cleanInput = () => {
+      setInput("")
+      setPostInput("")
+   }
+
    return (
       <>
-         {load && (
-            <>
-               <Sidebar triggerSidebar={sidebarSlice.sidebarSearch}>
-                  <div className="flex flex-col gap-3 p-[16px]">
-                     <div className="flex-center flex w-full gap-3">
-                        <Input getInput={getInput} postInput={postInput} />
-                        <AiOutlineClose
-                           onClick={() => dispatch(sidebarSearch(false))}
-                           className="h-[15px] w-[15px] cursor-pointer"
+         <>
+            <Sidebar triggerSidebar={sidebarSlice.sidebarSearch}>
+               <div className="flex flex-col gap-3 p-[16px]">
+                  <div className="flex-center flex w-full gap-3">
+                     <Input
+                        getInput={getInput}
+                        postInput={postInput}
+                        cleanInput={cleanInput}
+                     />
+                     <AiOutlineClose
+                        onClick={() => dispatch(sidebarSearch(false))}
+                        className="h-[15px] w-[15px] cursor-pointer"
+                     />
+                  </div>
+
+                  {input.length === 0 && (
+                     <div className="w-full">
+                        <TopSearches
+                           className="mt-3"
+                           getTopSearch={getTopSearch}
                         />
                      </div>
+                  )}
 
-                     {input.length === 0 && (
-                        <div className="w-full">
-                           <TopSearches
-                              className="mt-3"
-                              getTopSearch={getTopSearch}
-                           />
+                  {input.length > 0 && (
+                     <div>
+                        {input.length > 0 &&
+                        (data?.products.length || data?.products.length) ? (
+                           <p className="text-p">
+                              {data?.products.length + data?.products.length}{" "}
+                              result
+                              {data?.products.length + data?.products.length > 1
+                                 ? "s"
+                                 : ""}
+                              {` "${input}"`}
+                           </p>
+                        ) : (
+                           <p className="text-p">
+                              No results found for “{input}”. Maybe these will
+                              interest you...
+                           </p>
+                        )}
+                     </div>
+                  )}
+               </div>
+
+               {isSuccess && (
+                  <>
+                     {data.blogs.length > 0 && data.products.length > 0 ? (
+                        <div
+                           className="grid grid-cols-2 gap-4 overflow-auto px-[16px] pb-[16px]"
+                           onClick={() => {
+                              dispatch(sidebarSearch(false))
+                           }}
+                        >
+                           {data.products.map((product) => {
+                              return (
+                                 <Card
+                                    key={product.id}
+                                    btn
+                                    win
+                                    stars
+                                    rating
+                                    product={product}
+                                    imageAnimated
+                                    className="bg-white"
+                                 />
+                              )
+                           })}
+
+                           {data.blogs.map((blog) => (
+                              <BlogCard
+                                 key={blog.id}
+                                 extra="Read Me"
+                                 id={blog.id}
+                                 images={blog.images}
+                                 title={blog.title}
+                              />
+                           ))}
+                        </div>
+                     ) : (
+                        <div
+                           className="grid grid-cols-2 gap-4 overflow-auto px-[16px] pb-[16px]"
+                           onClick={() => {
+                              dispatch(sidebarSearch(false))
+                           }}
+                        >
+                           {emptyFilterData?.products.map((product) => {
+                              return (
+                                 <Card
+                                    key={product.id}
+                                    btn
+                                    win
+                                    stars
+                                    rating
+                                    product={product}
+                                    imageAnimated
+                                    className="bg-white"
+                                 />
+                              )
+                           })}
+
+                           {emptyFilterData?.blogs.map((blog) => (
+                              <BlogCard
+                                 key={blog.id}
+                                 extra="Read Me"
+                                 id={blog.id}
+                                 images={blog.images}
+                                 title={blog.title}
+                              />
+                           ))}
                         </div>
                      )}
-
-                     {input.length > 0 && (
-                        <div>
-                           {input.length > 0 &&
-                           (products.length || blogs.length) ? (
-                              <p className="text-p">
-                                 {blogs.length + products.length} result
-                                 {blogs.length + products.length > 1 ? "s" : ""}
-                                 {` "${input}"`}
-                              </p>
-                           ) : (
-                              <p className="text-p">
-                                 No results found for “{input}”. Maybe these
-                                 will interest you...
-                              </p>
-                           )}
-                        </div>
-                     )}
+                  </>
+               )}
+               {isPending && (
+                  <div className="flex-center mt-[30%] flex w-full">
+                     <AiOutlineLoading3Quarters className="animate-spin text-[40px] text-pink-600" />
                   </div>
-
-                  <div
-                     className="grid grid-cols-2 gap-4 overflow-auto px-[16px] pb-[16px]"
-                     onClick={() => {
-                        dispatch(sidebarSearch(false))
-                     }}
-                  >
-                     {products.map((product) => {
-                        return (
-                           <Card
-                              key={product.id}
-                              btn
-                              win
-                              stars
-                              rating
-                              product={product}
-                              imageAnimated
-                              className="bg-white"
-                           />
-                        )
-                     })}
-
-                     {blogs.map((blog) => (
-                        <BlogCard
-                           key={blog.id}
-                           extra="Read Me"
-                           id={blog.id}
-                           images={blog.images}
-                           title={blog.title}
-                        />
-                     ))}
-                  </div>
-               </Sidebar>
-            </>
-         )}
+               )}
+            </Sidebar>
+         </>
          <CiSearch
             onClick={() => dispatch(sidebarSearch(true))}
             className={`cursor-pointer text-[30px] ${className}`}
