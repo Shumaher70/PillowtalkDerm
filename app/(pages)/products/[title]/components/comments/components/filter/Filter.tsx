53fc265503777e121e1c8dbd9e36344ad09972fe
oleg <olegvraby@gmail.com>
2024-03-06 16:56:12 +0200

    change:comment/Filter: added fetch data instead of storage

--- app/(pages)/products/[title]/components/comments/components/filter/Filter.tsx
+++ app/(pages)/products/[title]/components/comments/components/filter/Filter.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { IoIosArrowUp } from "react-icons/io"
 
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import Sort from "./components/Sort"
 import Rating from "./components/Rating"
 import CheckBox from "./components/CheckBox"
@@ -9,14 +9,12 @@ import CheckBox from "./components/CheckBox"
 import { useAppDispatch, useAppSelector } from "@/redux/hooks"
 import axios from "axios"
 import Comment from "../comment/Comment"
-import {
-   dataReviewAction,
-   takeAction,
-} from "@/redux/features/commentFilterSlice"
+import { takeAction } from "@/redux/features/commentFilterSlice"
 
 import { ImSpinner8 } from "react-icons/im"
 import Image from "next/image"
 import { useQuery } from "@tanstack/react-query"
+import { Review } from "@prisma/client"
 
 const Filter = ({ id }: { id: string }) => {
    const filterSlice = useAppSelector((state) => state.commentFilterReducer)
@@ -25,16 +23,15 @@ const Filter = ({ id }: { id: string }) => {
    const dispatch = useAppDispatch()
 
    const getFilter = async () => {
-      await axios
+      const dynamicQuery: Review[] = await axios
          .get(
             `/api/reviewsFilter?sort=${filterSlice.sort}&media=${filterSlice.media}&stars=${filterSlice.stars}&take=${filterSlice.take}&id=${id}`
          )
-         .then((response) => {
-            dispatch(dataReviewAction(response.data))
-         })
+         .then((response) => response.data)
+      return dynamicQuery
    }
 
-   const { isLoading } = useQuery({
+   const { isLoading, data, isFetched } = useQuery({
       queryKey: [
          filterSlice.sort,
          filterSlice.media,
@@ -46,61 +43,59 @@ const Filter = ({ id }: { id: string }) => {
 
    return (
       <>
-         {filterSlice.review?.length! > 0 && (
-            <>
-               <div className="flex flex-col gap-5">
-                  <div
-                     onClick={() => setShow((previous) => !previous)}
-                     className="flex cursor-pointer items-center gap-1"
-                  >
-                     <div className="text-p text-[#6a1ba6]">Sort & Filter</div>
-                     <IoIosArrowUp
-                        className={`text-p text-[#6a1ba6] ${
-                           !show && "rotate-180"
-                        }`}
-                     />
-                  </div>
-
-                  <div className={`filter ${show ? "flex" : "!hidden"} gap-5`}>
-                     <Sort />
-                     <Rating />
-                     <CheckBox />
-                  </div>
+         <>
+            <div className="flex flex-col gap-5">
+               <div
+                  onClick={() => setShow((previous) => !previous)}
+                  className="flex cursor-pointer items-center gap-1"
+               >
+                  <div className="text-p text-[#6a1ba6]">Sort & Filter</div>
+                  <IoIosArrowUp
+                     className={`text-p text-[#6a1ba6] ${
+                        !show && "rotate-180"
+                     }`}
+                  />
                </div>
 
-               <div className="mt-5 flex flex-col gap-5">
-                  {!isLoading && (
-                     <>
-                        {filterSlice.review?.map((comment) => (
-                           <div key={comment.id}>
-                              <Comment review={comment} />
-                           </div>
-                        ))}
-                     </>
-                  )}
-                  {isLoading && (
-                     <div className="flex-center flex w-full">
-                        <div className="flex-center flex gap-x-2 bg-black/40 p-2 px-5">
-                           <p className="text-[20px] text-white">Loading...</p>
-                           <ImSpinner8 className="h-[20px] w-[20px] animate-spin text-white" />
+               <div className={`filter ${show ? "flex" : "!hidden"} gap-5`}>
+                  <Sort />
+                  <Rating />
+                  <CheckBox />
+               </div>
+            </div>
+
+            <div className="mt-5 flex flex-col gap-5">
+               {!isLoading && (
+                  <>
+                     {data?.map((comment) => (
+                        <div key={comment.id}>
+                           <Comment review={comment} />
                         </div>
+                     ))}
+                  </>
+               )}
+               {isLoading && (
+                  <div className="flex-center flex w-full">
+                     <div className="flex-center flex gap-x-2 bg-black/40 p-2 px-5">
+                        <p className="text-[20px] text-white">Loading...</p>
+                        <ImSpinner8 className="h-[20px] w-[20px] animate-spin text-white" />
                      </div>
-                  )}
-                  {filterSlice.review?.length! > 4 && (
-                     <div className="flex-center flex w-full">
-                        <button
-                           className="rounded-full bg-purple-700 px-[20px] py-[5px] uppercase text-white"
-                           onClick={() => dispatch(takeAction())}
-                        >
-                           see more reviews
-                        </button>
-                     </div>
-                  )}
-               </div>
-            </>
-         )}
+                  </div>
+               )}
+               {data?.length! > 4 && (
+                  <div className="flex-center flex w-full">
+                     <button
+                        className="rounded-full bg-purple-700 px-[20px] py-[5px] uppercase text-white"
+                        onClick={() => dispatch(takeAction())}
+                     >
+                        see more reviews
+                     </button>
+                  </div>
+               )}
+            </div>
+         </>
 
-         {filterSlice.review?.length === 0 && (
+         {isFetched && data?.length === 0 && (
             <>
                <div className="flex-center mt-10 flex w-full">
                   <Image
