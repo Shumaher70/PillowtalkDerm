aaeb5d343c628a53bc72b5f040c4704ca0139ab8
oleg <olegvraby@gmail.com>
2024-03-06 11:12:32 +0200

    create:@users/page: create parallel route with logic "Create a new user if the user doesn't exist in db"

---
+++ app/(pages)/@users/page.tsx
@@ -0,0 +1,29 @@
+import { prisma } from "@/lib/prisma"
+import { currentUser } from "@clerk/nextjs"
+
+const page = async () => {
+   const user = await currentUser()
+   if (user) {
+      const findUser = await prisma.user.findUnique({
+         where: {
+            id: user.id,
+         },
+      })
+
+      if (findUser === null) {
+         await prisma.user.create({
+            data: {
+               id: user.id,
+               first_name: user.firstName ?? "without first name",
+               last_name: user.lastName ?? "without last name",
+               email: user.emailAddresses[0].emailAddress,
+               img: user.imageUrl,
+            },
+         })
+      }
+   }
+
+   return null
+}
+
+export default page
--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -3,13 +3,16 @@ import Navbar from "../components/navbar/Navbar"
 
 export default function RootLayout({
    children,
+   users,
 }: {
    children: React.ReactNode
+   users: React.ReactNode
 }) {
    return (
       <>
          <Navbar />
          {children}
+         {users}
          <Footer />
       </>
    )
