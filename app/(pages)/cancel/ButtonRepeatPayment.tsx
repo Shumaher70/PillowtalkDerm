7220259342714470cc918d0de0e309abc107d891
oleg <olegvraby@gmail.com>
2024-03-06 09:44:48 +0200

    change:ButtonRepeatPayment: refactoring

--- app/(pages)/cancel/ButtonRepeatPayment.tsx
+++ app/(pages)/cancel/ButtonRepeatPayment.tsx
@@ -1,41 +1,14 @@
 "use client"
 
-import { useAppSelector } from "@/redux/hooks"
-import { useUser } from "@clerk/nextjs"
-import { loadStripe } from "@stripe/stripe-js"
-import axios from "axios"
 import { useRouter } from "next/navigation"
 
 const ButtonRepeatPayment = () => {
-   const cardSlice = useAppSelector((state) => state.cartReducer.carts)
-
-   const { isSignedIn } = useUser()
-
    const route = useRouter()
-
-   const handleClick = async () => {
-      const STRIPE_PK = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
-      const stripe = await loadStripe(STRIPE_PK)
-
-      const data = await axios
-         .post("/api/checkout", {
-            products: cardSlice,
-         })
-         .then((response) => response.data)
-
-      const sessionId = data.id
-
-      stripe?.redirectToCheckout({ sessionId })
-   }
 
    return (
       <button
          onClick={async () => {
-            if (isSignedIn) {
-               await handleClick()
-            } else {
-               route.push("sign-in")
-            }
+            route.back()
          }}
          className="flex-center !duration-250
    relative
